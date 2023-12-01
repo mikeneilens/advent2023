b2156a5f1548212440033b2bc1b5f96003c6b991
Mike Neilens <mike.neilens@gmail.com>
2023-12-01 10:01:42 +0000

    day one complete

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -3,9 +3,39 @@ fun main(args: Array<String>) {
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun String.firstDigit() = first{ it.isDigit() }.digitToInt()
+
+fun String.lastDigit() = last{ it.isDigit() }.digitToInt()
+
+fun String.calibrationValue() = firstDigit() * 10 + lastDigit()
+
+fun List<String>.calibrationValue() = sumOf(String::calibrationValue)
+
+fun partOne(sampleData:List<String>) :Int {
+    return sampleData.calibrationValue()
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+fun String.replaceNumericalText(numberMap:Pair<String, String>) = replace(numberMap.first, numberMap.second)
+
+fun String.replaceNumericalText(numberMap:List<Pair<String, String>> =
+                                    listOf("zero" to "0zero",
+                                        "one" to "o1ne",
+                                        "two" to "t2wo",
+                                        "three" to "t3hree",
+                                        "four" to "f4our",
+                                        "five" to "f5ive",
+                                        "six" to "s6ix",
+                                        "seven" to "s7even",
+                                        "eight" to "e8ight",
+                                        "nine" to "n9ine")
+):String =
+    if (numberMap.isEmpty()) this
+    else replaceNumericalText(numberMap.first()).replaceNumericalText(numberMap.drop(1))
+
+fun String.adjustedCalibrationValue() = replaceNumericalText().calibrationValue()
+
+fun List<String>.adjustedCalibrationValue() = sumOf(String::adjustedCalibrationValue)
+
+fun partTwo(sampleData:List<String>):Int {
+    return sampleData.adjustedCalibrationValue()
 }
--- day01/src/test/kotlin/MainTest.kt
+++ day01/src/test/kotlin/MainTest.kt
@@ -2,14 +2,64 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testData = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "first digit in string ab1c is 1" {
+            "ab1c".firstDigit() shouldBe  1
+        }
+        "last digit in string ab1c2d3e is 3" {
+            "ab1c2d3e".lastDigit() shouldBe  3
+        }
+        "calibration value in string ab1c2d3e is 13" {
+            "ab1c2d3e".calibrationValue() shouldBe  13
+        }
+        "calibration value for test data is 142" {
+            testData.calibrationValue() shouldBe  142
+        }
+        "part one should be 53921" {
+            partOne(sampleData) shouldBe  53921
         }
     })
+
+    val testData2 = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent().split("\n")
+
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+
+        "replacing digits in 4nineeightseven2 is 49nine8eight7seven2" {
+            "4nineeightseven2".replaceNumericalText() shouldBe "4n9inee8ights7even2"
+        }
+
+        "replacing digits in eightwothree is 49nine8eight7seven2" {
+            "eightwothree".replaceNumericalText() shouldBe "e8ight2wot3hree"
+        }
+
+        "adjusted calibration value for abcone2threexyz is 13 " {
+            "bcone2threexyz".adjustedCalibrationValue() shouldBe 13
+        }
+        "adjusted calibration value for eightwothree is 83 " {
+            "eightwothree".adjustedCalibrationValue() shouldBe 83
+        }
+        "adjusted calibration value for test data is 281" {
+            testData2.forEach { println(it.adjustedCalibrationValue()) }
+            testData2.adjustedCalibrationValue() shouldBe  281
+        }
+
+        "part two should be 54676" {
+            partTwo(sampleData) shouldBe  54676
         }
     })
 })
