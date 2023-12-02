40270c800fe205ee1435b0b9d35c14979ac4eb0c
Mike Neilens <mike.neilens@gmail.com>
2023-12-02 11:11:02 +0000

    day02 complete

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -1,11 +1,64 @@
-fun main(args: Array<String>) {
+fun main() {
     partOne(sampleData)
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+interface CubeType  {
+    val quantity:Int
+    val maxQuantity:Int
+    fun isValid() = quantity <= maxQuantity
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+data class RedCube(override val quantity:Int, override val maxQuantity:Int = 12):CubeType
+data class BlueCube(override val quantity:Int, override val maxQuantity:Int = 14):CubeType
+data class GreenCube(override val quantity:Int, override val maxQuantity: Int = 13):CubeType
+data class UnknownCube(override val quantity:Int = 0, override val maxQuantity: Int = 0):CubeType
+
+fun String.toCube() = when (this.split(" ").last()) {
+    "red" -> RedCube(this.split(" ").first().toInt())
+    "green" -> GreenCube(this.split(" ").first().toInt())
+    "blue" -> BlueCube(this.split(" ").first().toInt())
+    else -> UnknownCube()
 }
+
+data class Game(val gameNo:Int, val sets:List<List<CubeType>>) {
+    fun isValid() = sets.all(List<CubeType>::isValid)
+}
+
+fun partOne(sampleData:List<String>) :Int {
+    return sampleData.parse().validGameNumbers().sum()
+}
+
+fun List<String>.parse() = map(String::parse)
+
+fun String.parse() = Game(gameNo = toGameNo(), sets = toSets())
+
+fun String.toGameNo() = split(": ").first().removePrefix("Game ").toInt()
+
+fun String.toSets() = split(": ").last().split("; ").map(String::toCubes)
+
+fun String.toCubes():List<CubeType> = split(", ").map(String::toCube)
+
+fun List<Game>.validGameNumbers() = filter(Game::isValid).map{it.gameNo}
+
+fun List<CubeType>.isValid() = all(CubeType::isValid)
+
+fun partTwo(sampleData:List<String>):Int {
+    return sampleData.parse().power().sum()
+}
+
+fun List<Game>.power() = map(Game::power)
+
+fun Game.power() = minimumNoOfReds() * minimumNoOfGreens() * minimumNoOfBlues()
+
+fun Game.minimumNoOfReds() = sets.maxOf(List<CubeType>::minimumNoOfReds)
+
+fun Game.minimumNoOfGreens() = sets.maxOf(List<CubeType>::minimumNoOfGreens)
+
+fun Game.minimumNoOfBlues() = sets.maxOf(List<CubeType>::minimumNoOfBlues)
+
+fun List<CubeType>.minimumNoOfReds():Int = filter{it is RedCube}.maxByOrNull { it.quantity }?.quantity ?: 0
+
+fun List<CubeType>.minimumNoOfGreens():Int = filter{it is GreenCube}.maxByOrNull { it.quantity }?.quantity ?: 0
+
+fun List<CubeType>.minimumNoOfBlues():Int = filter{it is BlueCube}.maxByOrNull { it.quantity }?.quantity ?: 0
--- day02/src/test/kotlin/MainTest.kt
+++ day02/src/test/kotlin/MainTest.kt
@@ -2,14 +2,84 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testData = """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "parse string starting with 'Game 1: ' to 1"{
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".toGameNo() shouldBe 1
+        }
+        "parse string starting with 'Game 12: ' to 12"{
+            "Game 12: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".toGameNo() shouldBe 12
+        }
+        "parse a string containing Game 1: 3 blue, 4 red into a list of Blue(3),Red(4)" {
+            "Game 1: 3 blue, 4 red".toSets() shouldBe listOf(listOf(BlueCube(3),RedCube(4)))
+        }
+
+        "parse a string into a game" {
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".parse() shouldBe
+                    Game(gameNo = 1,
+                         sets = listOf(listOf(BlueCube(3), RedCube(4)),
+                                listOf( RedCube(1), GreenCube(2), BlueCube(6)),
+                                listOf(GreenCube(2))))
+        }
+
+        "red cube with quantity of 12 is valid" {
+            RedCube(12).isValid() shouldBe true
+        }
+        "red cube with quantity of 13 is not valid" {
+            println(RedCube(13))
+            RedCube(13).isValid() shouldBe false
+        }
+        "a list of cubes containing a red cube with quantity 12 is valid" {
+            val cubes = listOf(RedCube(12),GreenCube(0),BlueCube(0))
+            cubes.isValid() shouldBe true
+        }
+        "a list of cubes containing a blue cube with quantity 15 is not valid" {
+            val cubes = listOf(RedCube(12),GreenCube(0),BlueCube(15))
+            cubes.isValid() shouldBe false
+        }
+
+        "parse the test data correctly" {
+            val result = testData.parse()
+            result.size shouldBe 5
+            result[0].gameNo shouldBe 1
+            result[0].sets[0] shouldBe listOf(BlueCube(3),RedCube(4))
+            result[0].sets[1] shouldBe listOf(RedCube(1),GreenCube(2),BlueCube(6))
+            result[0].sets[2] shouldBe listOf(GreenCube(2))
+            result[4].gameNo shouldBe 5
+            result[4].sets[0] shouldBe listOf(RedCube(6),BlueCube(1),GreenCube(3))
+            result[4].sets[1] shouldBe listOf(BlueCube(2),RedCube(1),GreenCube(2))
+        }
+        "valid game numbers in the test data are 1, 2, 5" {
+            testData.parse().validGameNumbers() shouldBe listOf(1, 2, 5)
+        }
+        "part one using test data should be 8" {
+            partOne(testData) shouldBe 8
+        }
+        "part one using sample data should be 2449" {
+            partOne(sampleData) shouldBe 2449
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        "In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes." {
+            testData.parse().first().minimumNoOfReds() shouldBe 4
+            testData.parse().first().minimumNoOfGreens() shouldBe 2
+            testData.parse().first().minimumNoOfBlues() shouldBe 6
+        }
+        "power of each set of cubes in the test data should be 48, 12, 1560, 630, and 36" {
+            testData.parse().power() shouldBe listOf(48, 12, 1560, 630, 36)
+        }
+        "part two using test data should be 2286" {
+            partTwo(testData) shouldBe 2286
+        }
+        "part two using sample data should be 63981" {
+            partTwo(sampleData) shouldBe 63981
         }
     })
 })
