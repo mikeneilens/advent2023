8edf8fb61f30df9a06ffbc44820354c30f7e2416
Mike Neilens <mike.neilens@gmail.com>
2023-12-03 11:59:22 +0000

    day03 part one

--- day03/src/main/kotlin/Main.kt
+++ day03/src/main/kotlin/Main.kt
@@ -1,11 +1,55 @@
-fun main(args: Array<String>) {
+fun main() {
     partOne(sampleData)
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+data class NumberChunk(val startCol:Int, var endCol:Int, val row:Int, val data:List<String>) {
+
+    fun adjacentPositions() =
+        ((startCol - 1)..(endCol + 1 )).flatMap{ x ->
+            ((row - 1)..(row + 1 )).map{ y -> Position(y,x) }
+        }.filter {it.isInRange(data.maxRow, data.maxCol) && !it.overlapsNumberChunk(this) }
+
+    fun value() = data[row].substring(startCol..endCol).toInt()
+
+    fun isAdjacentToSymbol() = adjacentPositions().any { it.containsSymbol(data) }
 }
-fun partTwo(sampleData:String):Int {
+
+data class Position(val row:Int, val col:Int) {
+
+    fun isInRange(maxRow:Int, maxCol:Int) = row in 0..maxRow && col in 0..maxCol
+
+    fun overlapsNumberChunk(numberChunk: NumberChunk) = row == numberChunk.row && col in (numberChunk.startCol..numberChunk.endCol)
+
+    fun containsSymbol(data:List<String>) = data[row][col] !in '0'..'9' && data[row][col] != '.'
+}
+
+val List<String>.maxCol get() = first().length - 1
+val List<String>.maxRow get() = size - 1
+
+fun partOne(sampleData:List<String>) :Int {
+    return sampleData.toNumberChunks().adjacentToSymbols().sumOf { it.value() }
+}
+
+fun List<String>.toNumberChunks() = flatMapIndexed{ i, s -> s.toNumberChunks(i, this)}
+
+fun String.toNumberChunks(row:Int, data:List<String>) =
+    foldIndexed(listOf(null)){col, list:List<NumberChunk?>, newItem ->
+        if (newItem in '0'..'9') {
+            val lastItem:NumberChunk? = list.last()
+            if (lastItem == null) {
+                list + NumberChunk(startCol = col, endCol = col, row = row, data = data)
+            } else {
+                lastItem.endCol = col
+                list
+            }
+        } else {
+            list + null
+        }
+    }.filterNotNull()
+
+fun List<NumberChunk>.adjacentToSymbols() = filter{it.isAdjacentToSymbol() }
+
+fun partTwo(sampleData:List<String>):Int {
     return 0
 }
--- day03/src/test/kotlin/MainTest.kt
+++ day03/src/test/kotlin/MainTest.kt
@@ -2,9 +2,92 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+
+    val testData = """
+                467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...${'$'}.*....
+                .664.598..
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "calculate positions adjacent to chunk(startcol=2, endCol=3, row=2) when not at end of row or column" {
+            val numberChunk = NumberChunk(startCol = 2, endCol = 3, row = 2, data = testData)
+            val expectedPositions = setOf(
+                Position(1,1),
+                Position(1,2),
+                Position(1,3),
+                Position(1,4),
+                Position(2,1),
+                Position(2,4),
+                Position(3,1),
+                Position(3,2),
+                Position(3,3),
+                Position(3,4),
+            )
+            numberChunk.adjacentPositions().toSet() shouldBe expectedPositions
+        }
+        "calculate positions adjacent to chunk(startcol=8, endCol=9, row=2) when is at end of row" {
+            val numberChunk = NumberChunk(startCol = 8, endCol = 9, row = 2, testData)
+            val expectedPositions = setOf(
+                Position(1,7),
+                Position(1,8),
+                Position(1,9),
+                Position(2,7),
+                Position(3,7),
+                Position(3,8),
+                Position(3,9),
+            )
+            numberChunk.adjacentPositions().toSet() shouldBe expectedPositions
+        }
+        "calculate positions adjacent to chunk(startcol=2, endCol=3, row=9) when at end of column" {
+            val numberChunk = NumberChunk(startCol = 2, endCol = 3, row = 9, testData)
+            val expectedPositions = setOf(
+                Position(8,1),
+                Position(8,2),
+                Position(8,3),
+                Position(8,4),
+                Position(9,1),
+                Position(9,4),
+            )
+            numberChunk.adjacentPositions().toSet() shouldBe expectedPositions
+        }
+        "convert '..592.....' on row 5 to listOf(numberchunk(startcol=2, endCol=4,row=5))" {
+            "..592.....".toNumberChunks(row=5, testData) shouldBe listOf(NumberChunk(2,4,5, testData))
+        }
+
+        "convert '467..114..' on row 5 to list containing two NumberChunk's" {
+            val expectedFirstChunk = NumberChunk(0,2,5, testData)
+            val expectedSecondChunk = NumberChunk(5,7,5, testData)
+            "467..114..".toNumberChunks(row=5, testData) shouldBe listOf(expectedFirstChunk, expectedSecondChunk)
+        }
+
+        "value of numberChunk(startCol = 6, endCol = 8, row = 2) in testData is 633" {
+            NumberChunk(startCol = 6, endCol = 8, row = 2, testData).value() shouldBe 633
+        }
+        "numberChunks adjacent to symbols in the test datat" {
+            val expectedResult = listOf(
+                NumberChunk(startCol=0, endCol=2, row=0, testData),
+                NumberChunk(startCol=2, endCol=3, row=2, testData),
+                NumberChunk(startCol=6, endCol=8, row=2, testData),
+                NumberChunk(startCol=0, endCol=2, row=4, testData),
+                NumberChunk(startCol=2, endCol=4, row=6, testData),
+                NumberChunk(startCol=6, endCol=8, row=7, testData),
+                NumberChunk(startCol=1, endCol=3, row=9, testData),
+                NumberChunk(startCol=5, endCol=7, row=9, testData))
+            testData.toNumberChunks().adjacentToSymbols() shouldBe expectedResult
+        }
+        "part one with test data should be 4361" {
+            partOne(testData) shouldBe 4361
+        }
+        "part one should be 538046" {
+            partOne(sampleData) shouldBe 538046
         }
     })
     "part two" should ({
