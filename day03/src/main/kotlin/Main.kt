454a32ae7f08f09c8108eed832eafb6994e148ea
Mike Neilens <mike.neilens@gmail.com>
2023-12-03 15:33:47 +0000

    day03 refactor

--- day03/src/main/kotlin/Main.kt
+++ day03/src/main/kotlin/Main.kt
@@ -3,73 +3,84 @@ fun main() {
     partTwo(sampleData)
 }
 
-data class NumberChunk(val startCol:Int, var endCol:Int, val row:Int, val data:List<String>) {
+data class NumberChunk(val columnRange:IntRange, val row:Int, val data:List<String>) {
 
     fun adjacentPositions() =
-        ((startCol - 1)..(endCol + 1 )).flatMap{ x ->
-            ((row - 1)..(row + 1 )).map{ y -> Position(y,x) }
-        }.filter {it.isInRange(data.maxRow, data.maxCol) && !it.overlapsNumberChunk(this) }
+        (columnRange.widenBy(1 )).flatMap{ x ->
+            ((row..row).widenBy(1)).map{ y -> Position(y,x) }
+        }.filter {it.isInRange(data) && !it.overlapsNumberChunk(this) }
 
-    fun value() = data[row].substring(startCol..endCol).toInt()
+    fun extendWidth() = this.copy(columnRange = columnRange.first..(columnRange.last + 1))
 
-    fun isAdjacentToSymbol() = adjacentPositions().any { it.containsSymbol(data) }
+    fun value() = data[row].substring(columnRange).toInt()
+
+    fun isAdjacentToSymbol() = adjacentPositions().any {position -> position.containsSymbol(data) }
 
     fun adjacentPositionsContainingAsterisk() = adjacentPositions().filter {data[it.row][it.col] == '*'}
 }
 
+fun IntRange.widenBy(r:Int) = (this.first - r).. (this.last + r)
+
 data class Position(val row:Int, val col:Int) {
 
-    fun isInRange(maxRow:Int, maxCol:Int) = row in 0..maxRow && col in 0..maxCol
+    fun isInRange(data:List<String>) = row in data.rowRange && col in data.colRange
 
-    fun overlapsNumberChunk(numberChunk: NumberChunk) = row == numberChunk.row && col in (numberChunk.startCol..numberChunk.endCol)
+    fun overlapsNumberChunk(numberChunk: NumberChunk) = row == numberChunk.row && col in numberChunk.columnRange
 
-    fun containsSymbol(data:List<String>) = data[row][col] !in '0'..'9' && data[row][col] != '.'
+    fun containsSymbol(data:List<String>) = data[row][col].isSymbol()
 }
 
-val List<String>.maxCol get() = first().length - 1
-val List<String>.maxRow get() = size - 1
+val List<String>.colRange get() = 0..(first().lastIndex)
+val List<String>.rowRange get() = 0.. (lastIndex)
+
+fun Char.isSymbol() = !(this.isDigit() || this == '.')
 
 fun partOne(sampleData:List<String>) :Int {
-    return sampleData.toNumberChunks().adjacentToSymbols().sumOf { it.value() }
+    return sampleData.toNumberChunks()
+        .chunksAdjacentToSymbols()
+        .sumOf { it.value() }
 }
 
 fun List<String>.toNumberChunks() = flatMapIndexed{ i, s -> s.toNumberChunks(i, this)}
 
 fun String.toNumberChunks(row:Int, data:List<String>) =
-    foldIndexed(listOf(null)){col, list:List<NumberChunk?>, newItem ->
-        if (newItem in '0'..'9') {
-            val lastItem:NumberChunk? = list.last()
-            if (lastItem == null) {
-                list + NumberChunk(startCol = col, endCol = col, row = row, data = data)
-            } else {
-                lastItem.endCol = col
-                list
-            }
+    foldIndexed(mutableListOf<NumberChunk?>()){col, list, newItem ->
+        if (newItem.isDigit()) {
+            list.insertOrUpdateLastNumberChunk(col, row, data)
         } else {
-            list + null
+            list.add(null)
         }
+        list
     }.filterNotNull()
 
-fun List<NumberChunk>.adjacentToSymbols() = filter{it.isAdjacentToSymbol() }
+fun MutableList<NumberChunk?>.insertOrUpdateLastNumberChunk(col: Int, row: Int, data: List<String>) {
+    if (isEmpty() || last() == null) {
+        add(NumberChunk(columnRange = col.. col, row = row, data = data))
+    } else {
+        this[lastIndex] = last()?.extendWidth()
+    }
+}
+
+fun List<NumberChunk>.chunksAdjacentToSymbols() = filter{it.isAdjacentToSymbol() }
 
 fun partTwo(sampleData:List<String>):Int {
     val asteriskMap = mutableMapOf<Position,List<NumberChunk>>()
-    sampleData.toNumberChunks().updateAsteriskMap(asteriskMap)
+    asteriskMap.updateAsteriskMap(sampleData.toNumberChunks())
     return asteriskMap.gearRatios().sum()
 }
 
-fun List<NumberChunk>.updateAsteriskMap(map:MutableMap<Position, List<NumberChunk>>) {
-    forEach { numberChunk ->
+fun MutableMap<Position, List<NumberChunk>>.updateAsteriskMap(numberChunks: List<NumberChunk>) {
+    numberChunks.forEach { numberChunk ->
         numberChunk.adjacentPositionsContainingAsterisk().forEach {position ->
-            val currentNumberChunks = map[position] ?: listOf()
-            map[position] = currentNumberChunks + numberChunk
+            val currentNumberChunks = get(position) ?: listOf()
+            set(position, currentNumberChunks + numberChunk)
         }
     }
 }
 
 fun Map<Position, List<NumberChunk>>.gearRatios() =
-    toList().mapNotNull{ (position, numberChunks) ->
+    toList().mapNotNull{ (_, numberChunks) ->
         if (numberChunks.size == 2)
-            numberChunks.fold(1){a,v -> a * v.value()}
+            numberChunks.fold(1){ result, numberChunk -> result * numberChunk.value()}
             else null
     }
--- day03/src/test/kotlin/MainTest.kt
+++ day03/src/test/kotlin/MainTest.kt
@@ -18,7 +18,7 @@ class MainTest:WordSpec({
 
     "part one" should ({
         "calculate positions adjacent to chunk(startcol=2, endCol=3, row=2) when not at end of row or column" {
-            val numberChunk = NumberChunk(startCol = 2, endCol = 3, row = 2, data = testData)
+            val numberChunk = NumberChunk(columnRange = 2..3, row = 2, data = testData)
             val expectedPositions = setOf(
                 Position(1,1),
                 Position(1,2),
@@ -34,7 +34,7 @@ class MainTest:WordSpec({
             numberChunk.adjacentPositions().toSet() shouldBe expectedPositions
         }
         "calculate positions adjacent to chunk(startcol=8, endCol=9, row=2) when is at end of row" {
-            val numberChunk = NumberChunk(startCol = 8, endCol = 9, row = 2, testData)
+            val numberChunk = NumberChunk(columnRange = 8..9, row = 2, testData)
             val expectedPositions = setOf(
                 Position(1,7),
                 Position(1,8),
@@ -47,7 +47,7 @@ class MainTest:WordSpec({
             numberChunk.adjacentPositions().toSet() shouldBe expectedPositions
         }
         "calculate positions adjacent to chunk(startcol=2, endCol=3, row=9) when at end of column" {
-            val numberChunk = NumberChunk(startCol = 2, endCol = 3, row = 9, testData)
+            val numberChunk = NumberChunk(columnRange = 2..3, row = 9, testData)
             val expectedPositions = setOf(
                 Position(8,1),
                 Position(8,2),
@@ -59,29 +59,29 @@ class MainTest:WordSpec({
             numberChunk.adjacentPositions().toSet() shouldBe expectedPositions
         }
         "convert '..592.....' on row 5 to listOf(numberchunk(startcol=2, endCol=4,row=5))" {
-            "..592.....".toNumberChunks(row=5, testData) shouldBe listOf(NumberChunk(2,4,5, testData))
+            "..592.....".toNumberChunks(row=5, testData) shouldBe listOf(NumberChunk(2..4,5, testData))
         }
 
         "convert '467..114..' on row 5 to list containing two NumberChunk's" {
-            val expectedFirstChunk = NumberChunk(0,2,5, testData)
-            val expectedSecondChunk = NumberChunk(5,7,5, testData)
+            val expectedFirstChunk = NumberChunk(0..2,5, testData)
+            val expectedSecondChunk = NumberChunk(5..7,5, testData)
             "467..114..".toNumberChunks(row=5, testData) shouldBe listOf(expectedFirstChunk, expectedSecondChunk)
         }
 
         "value of numberChunk(startCol = 6, endCol = 8, row = 2) in testData is 633" {
-            NumberChunk(startCol = 6, endCol = 8, row = 2, testData).value() shouldBe 633
+            NumberChunk(columnRange = 6..8, row = 2, testData).value() shouldBe 633
         }
         "numberChunks adjacent to symbols in the test datat" {
             val expectedResult = listOf(
-                NumberChunk(startCol=0, endCol=2, row=0, testData),
-                NumberChunk(startCol=2, endCol=3, row=2, testData),
-                NumberChunk(startCol=6, endCol=8, row=2, testData),
-                NumberChunk(startCol=0, endCol=2, row=4, testData),
-                NumberChunk(startCol=2, endCol=4, row=6, testData),
-                NumberChunk(startCol=6, endCol=8, row=7, testData),
-                NumberChunk(startCol=1, endCol=3, row=9, testData),
-                NumberChunk(startCol=5, endCol=7, row=9, testData))
-            testData.toNumberChunks().adjacentToSymbols() shouldBe expectedResult
+                NumberChunk(columnRange=0..2, row=0, testData),
+                NumberChunk(columnRange=2..3, row=2, testData),
+                NumberChunk(columnRange=6..8, row=2, testData),
+                NumberChunk(columnRange=0..2, row=4, testData),
+                NumberChunk(columnRange=2..4, row=6, testData),
+                NumberChunk(columnRange=6..8, row=7, testData),
+                NumberChunk(columnRange=1..3, row=9, testData),
+                NumberChunk(columnRange=5..7, row=9, testData))
+            testData.toNumberChunks().chunksAdjacentToSymbols() shouldBe expectedResult
         }
         "part one with test data should be 4361" {
             partOne(testData) shouldBe 4361
@@ -93,7 +93,7 @@ class MainTest:WordSpec({
     "part two" should ({
         "update an astrisk map showing number chunks for each asterisk position in the test data" {
             val asteriskMap = mutableMapOf<Position,List<NumberChunk>>()
-            testData.toNumberChunks().updateAsteriskMap(asteriskMap)
+            asteriskMap.updateAsteriskMap(testData.toNumberChunks())
 
             val asteriskRow1Col3 = asteriskMap[Position(1,3)]
             asteriskRow1Col3?.size shouldBe 2
@@ -111,7 +111,7 @@ class MainTest:WordSpec({
         }
         "gear ratios in test data is [16345, 451490]" {
             val asteriskMap = mutableMapOf<Position,List<NumberChunk>>()
-            testData.toNumberChunks().updateAsteriskMap(asteriskMap)
+            asteriskMap.updateAsteriskMap(testData.toNumberChunks())
             asteriskMap.gearRatios() shouldBe listOf(16345, 451490)
         }
         "part two using test data should be 467835" {
