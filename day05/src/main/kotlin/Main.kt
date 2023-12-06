ffe587e98ffe62e08bf2f9cdc1d3e961fd78a9f7
Mike Neilens <mike.neilens@gmail.com>
2023-12-06 21:24:23 +0000

    day5 part two

--- day05/src/main/kotlin/Main.kt
+++ day05/src/main/kotlin/Main.kt
@@ -7,6 +7,15 @@ data class RangeOffset(val range:LongRange, val indexOffset:Long)
 
 data class ResourceMap(val rangeOffsets:List<RangeOffset>) {
     fun destinationIndex(sourceIndex:Long) = sourceIndex + (rangeOffsets.firstOrNull { sourceIndex in it.range }?.indexOffset ?: 0)
+
+    fun destinationRanges(sourceRanges:List<LongRange>):List<LongRange> {
+        val segmentedRanges = sourceRanges.segmentUsing(rangeOffsets)
+        return segmentedRanges.map{ segmentedRange ->
+            val indexOfOffset = rangeOffsets.indexOfFirst { segmentedRange inside it.range }
+            if (indexOfOffset >= 0) segmentedRange.start + rangeOffsets[indexOfOffset].indexOffset .. segmentedRange.last + rangeOffsets[indexOfOffset].indexOffset
+            else segmentedRange
+        }
+    }
 }
 
 fun String.toRangeOffset():RangeOffset = RangeOffset(range(), offset())
@@ -25,12 +34,37 @@ fun List<String>.toSeeds() = first().removePrefix("seeds: ").split(" ").map(Stri
 fun List<String>.toResourceMaps() = (0..6).map{toResourceMap(it)}
 
 fun List<String>.toResourceMap(n:Int):ResourceMap =
-    ResourceMap(joinToString("\n").split("\n\n")[n + 1].split("\n").drop(1).map(String::toRangeOffset))
+    ResourceMap(joinToString("\n").split("\n\n")[n + 1].split("\n").drop(1).map(String::toRangeOffset).sortedBy { it.range.start })
 
 tailrec fun seedToLocation(source:Long, resourceMaps:List<ResourceMap>):Long =
     if (resourceMaps.isEmpty()) source
     else seedToLocation(resourceMaps.first().destinationIndex(source),resourceMaps. drop(1))
 
 fun partTwo(sampleData:List<String>):Long {
-    return if (sampleData.isEmpty()) 0L else 0L - 0L
+    return seedToLocation(sampleData.toSeedRanges(), sampleData.toResourceMaps()).minOf { it.first }
 }
+
+fun List<String>.toSeedRanges() = first().removePrefix("seeds: ").split(" ")
+    .map(String::toLong).chunked(2).map{ LongRange(it.first(),(it.first() + it.last() - 1)) }
+
+fun seedToLocation(source:List<LongRange>, resourceMaps:List<ResourceMap>):List<LongRange> {
+    return if (resourceMaps.isEmpty()) source
+    else source.flatMap{range -> seedToLocation(resourceMaps.first().destinationRanges(listOf(range)),resourceMaps.drop(1))}
+}
+
+fun LongRange.segmentUsing(targetRange:LongRange):List<LongRange> {
+    if (before(targetRange) || after(targetRange) || inside(targetRange)) return listOf(this)
+    if (targetRange inside this) return listOf( start..(targetRange.start -1), targetRange, (targetRange.last + 1)..last )
+    if (start < targetRange.start) return listOf( start..(targetRange.start -1), targetRange.start..last  )
+    if (start > targetRange.start) return listOf( start..(targetRange.last), (targetRange.last + 1)..last  )
+    return listOf()
+}
+
+fun List<LongRange>.segmentUsing(rangeOffsets:List<RangeOffset>):List<LongRange> {
+    if (rangeOffsets.isEmpty()) return this
+    else return flatMap{it.segmentUsing(rangeOffsets.first().range)}.segmentUsing(rangeOffsets.drop(1))
+}
+
+infix fun LongRange.before(other:LongRange) = last < other.start
+infix fun LongRange.after(other:LongRange) = start > other.last
+infix fun LongRange.inside(other:LongRange) = start >= other.start && last <= other.last
--- day05/src/test/kotlin/MainTest.kt
+++ day05/src/test/kotlin/MainTest.kt
@@ -77,7 +77,7 @@ class MainTest:WordSpec({
         "create ResourceMap for seeds-to-soil from the test data" {
             val rangeOffset1 = RangeOffset(98L..99L, -48L  )
             val rangeOffset2 = RangeOffset(50L..97L, +2L  )
-            val seedResourceMap = ResourceMap(listOf(rangeOffset1, rangeOffset2))
+            val seedResourceMap = ResourceMap(listOf(rangeOffset2, rangeOffset1))
             testData.toResourceMap(0) shouldBe seedResourceMap
         }
         "using test data location for seed 79 is 82" {
@@ -95,8 +95,49 @@ class MainTest:WordSpec({
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        " 1..100 segmented using 101..200 gives [1..100]" {
+            (1L..100L).segmentUsing(101L..200L) shouldBe listOf(1L..100L)
+        }
+        " 201..300 segmented using 101..200 gives [1..100]" {
+            (201L..300L).segmentUsing(101L..200L) shouldBe listOf(201L..300L)
+        }
+        " 101..200 segmented using 101..200 gives [1..100]" {
+            (101L..200L).segmentUsing(101L..200L) shouldBe listOf(101L..200L)
+        }
+        " 102..199 segmented using 101..200 gives [1..100]" {
+            (102L..199L).segmentUsing(101L..200L) shouldBe listOf(102L..199L)
+        }
+        " 1..300 segmented using 101..200 gives [1..100, 101..200, 201..300]" {
+            (1L..300L).segmentUsing(101L..200L) shouldBe listOf(1..100L, 101..200L, 201..300L)
+        }
+        " 1..150 segmented using 101..200 gives [1..100, 101..150]" {
+            (1L..150L).segmentUsing(101L..200L) shouldBe listOf(1..100L, 101..150L)
+        }
+        " 150..300 segmented using 101..200 gives [150..200, 201..300]" {
+            (150L..300L).segmentUsing(101L..200L) shouldBe listOf(150..200L, 201..300L)
+        }
+        " 1..100 segmented using 20..30 and 40..50 gives 1..19, 20..30, 31..39, 40..50, 51..100" {
+            val rangeOffset1 = RangeOffset(20..30L, 1)
+            val rangeOffset2 = RangeOffset(40..50L, 2)
+            listOf(1..100L).segmentUsing(listOf(rangeOffset1, rangeOffset2)) shouldBe listOf(
+                1..19L, 20..30L, 31..39L, 40..50L, 51..100L
+            )
+        }
+        "testdata toSeedRanges is " {
+            testData.toSeedRanges() shouldBe listOf(79..92L, 55..67L)
+        }
+        "Locations for seed range 55..67L " {
+            seedToLocation(listOf(55..67L), testData.toResourceMaps()).minOf { it.first } shouldBe 56L
+        }
+        "Locations for seed range 79..92 " {
+            seedToLocation(listOf(79..92L), testData.toResourceMaps()).minOf { it.first } shouldBe 46L
+        }
+        "part two using test data should be 46" {
+            partTwo(testData) shouldBe 46
+        }
+        "part two using sample data should be 78775051" {
+            partTwo(sampleData) shouldBe 78775051
         }
     })
+
 })
