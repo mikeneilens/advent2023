632bc136b71a9add2483d04a097acad402237f74
Mike Neilens <mike.neilens@gmail.com>
2023-12-07 12:55:37 +0000

    day7 part two refactor

--- day07/src/main/kotlin/Main.kt
+++ day07/src/main/kotlin/Main.kt
@@ -6,24 +6,22 @@ fun main() {
 }
 
 enum class HandType(val value:Double) {
-    FiveOfAKind(6 * 13.0.pow(5)),
-    FourOfAKind(5 * 13.0.pow(5)),
-    FullHouse(4 * 13.0.pow(5)),
-    ThreeOfAKind(3 * 13.0.pow(5)),
-    TwoPair(2 * 13.0.pow(5)),
-    OnePair(13.0.pow(5)),
+    FiveOfAKind(6 * 14.0.pow(5)),
+    FourOfAKind(5 * 14.0.pow(5)),
+    FullHouse(4 * 14.0.pow(5)),
+    ThreeOfAKind(3 * 14.0.pow(5)),
+    TwoPair(2 * 14.0.pow(5)),
+    OnePair(14.0.pow(5)),
     HighCard(0.0)
 }
 
 data class Hand(val cards:String, val bid:Int = 0, val cardValues:Map<Char, Int> =
-    mapOf('A' to 12, 'K' to 11, 'Q' to 10, 'J' to 9, 'T' to 8, '9' to 7, '8' to 6, '7' to 5, '6' to 4, '5' to 3, '4' to 2, '3' to 1, '2' to 0)
+    mapOf('A' to 13, 'K' to 12, 'Q' to 11, 'J' to 10, 'T' to 9, '9' to 8, '8' to 7, '7' to 6, '6' to 5, '5' to 4, '4' to 3, '3' to 2, '2' to 1, '*' to 0)
     ) {
 
     fun totalScore() = handType().value + cardsScore()
 
-    fun totalScoreP2() = handTypeP2().value + cardsScore()
-
-    fun cardsScore() = cards.reversed().mapIndexed{n, c -> 13.0.pow(n) * cardValues.getValue(c)}.sum()
+    fun cardsScore() = cards.reversed().mapIndexed{n, c -> 14.0.pow(n) * cardValues.getValue(c)}.sum()
 
     fun toMappedCards(map:MutableMap<Char, Int> = mutableMapOf()):Map<Char, Int> {
         cards.forEach{c -> map[c] = (map[c] ?: 0) + 1}
@@ -32,22 +30,12 @@ data class Hand(val cards:String, val bid:Int = 0, val cardValues:Map<Char, Int>
 
     fun handType() = when(toMappedCards().values.sorted()) {
         listOf(5) -> HandType.FiveOfAKind
-        listOf(1,4) -> HandType.FourOfAKind
-        listOf(2,3) -> HandType.FullHouse
-        listOf(1,1,3) -> HandType.ThreeOfAKind
-        listOf(1,2,2) -> HandType.TwoPair
-        listOf(1,1,1,2) -> HandType.OnePair
-        else -> HandType.HighCard
-    }
-
-    fun handTypeP2() = when(toMappedCards().values.sorted()) {
-        listOf(5) -> HandType.FiveOfAKind
-        listOf(1,4) -> if(cards.contains('J')) HandType.FiveOfAKind else HandType.FourOfAKind
-        listOf(2,3) -> if(cards.contains('J')) HandType.FiveOfAKind else HandType.FullHouse
-        listOf(1,1,3) -> if(cards.quantityOf('J') == 1  || cards.filter{it == 'J'}.length == 3) HandType.FourOfAKind else HandType.ThreeOfAKind
-        listOf(1,2,2) -> if(cards.quantityOf('J') == 2) HandType.FourOfAKind else if(cards.quantityOf('J') == 1) HandType.FullHouse else HandType.TwoPair
-        listOf(1,1,1,2) -> if(cards.quantityOf('J') in 1..2) HandType.ThreeOfAKind else HandType.OnePair
-        else -> if(cards.quantityOf('J') == 1) HandType.OnePair else HandType.HighCard
+        listOf(1,4) -> if(cards.contains('*')) HandType.FiveOfAKind else HandType.FourOfAKind
+        listOf(2,3) -> if(cards.contains('*')) HandType.FiveOfAKind else HandType.FullHouse
+        listOf(1,1,3) -> if(cards.quantityOf('*') == 1  || cards.filter{it == '*'}.length == 3) HandType.FourOfAKind else HandType.ThreeOfAKind
+        listOf(1,2,2) -> if(cards.quantityOf('*') == 2) HandType.FourOfAKind else if(cards.quantityOf('*') == 1) HandType.FullHouse else HandType.TwoPair
+        listOf(1,1,1,2) -> if(cards.quantityOf('*') in 1..2) HandType.ThreeOfAKind else HandType.OnePair
+        else -> if(cards.quantityOf('*') == 1) HandType.OnePair else HandType.HighCard
     }
 }
 
@@ -63,10 +51,9 @@ fun partTwo(sampleData:List<String>):Int {
     return sampleData.toHandsP2().winningsP2()
 }
 
-fun List<Hand>.winningsP2() = sortedBy { it.totalScoreP2() }.mapIndexed { i, c -> Pair((i + 1), c) }.sumOf { it.first * it.second.bid }
+fun List<Hand>.winningsP2() = sortedBy { it.totalScore() }.mapIndexed { i, c -> Pair((i + 1), c) }.sumOf { it.first * it.second.bid }
 
-fun List<String>.toHandsP2(cardValues:Map<Char, Int> = mapOf(
-    'A' to 12, 'K' to 11, 'Q' to 10, 'J' to 0, 'T' to 9, '9' to 8, '8' to 7, '7' to 6, '6' to 5, '5' to 4, '4' to 3, '3' to 2, '2' to 1)) =
-    map{Hand(it.split(" ")[0], it.split(" ")[1].toInt(), cardValues)}
+fun List<String>.toHandsP2() =
+    map{Hand(it.split(" ")[0].replace('J','*'), it.split(" ")[1].toInt())}
 
-fun String.quantityOf(c:Char) = filter{it == 'J'}.length
+fun String.quantityOf(c:Char) = filter{it == c}.length
--- day07/src/test/kotlin/MainTest.kt
+++ day07/src/test/kotlin/MainTest.kt
@@ -10,14 +10,14 @@ class MainTest:WordSpec({
         QQQJA 483
     """.trimIndent().split("\n")
     "part one" should ({
-        "cardsScore of cards 23 is 1" {
-            Hand("23").cardsScore() shouldBe 1.0
+        "cardsScore of cards 23 is 16" {
+            Hand("23").cardsScore() shouldBe 16.0
         }
-        "cardsScore of cards 33 is 14" {
-            Hand("33").cardsScore() shouldBe 14.0
+        "cardsScore of cards 33 is 30" {
+            Hand("33").cardsScore() shouldBe 30.0
         }
-        "cardsScore of cards 43 is 27" {
-            Hand("43").cardsScore() shouldBe 27.0
+        "cardsScore of cards 43 is 44" {
+            Hand("43").cardsScore() shouldBe 44.0
         }
         "AAAAA is five of a kind" {
             Hand("AAAAA").handType() shouldBe HandType.FiveOfAKind
@@ -48,49 +48,49 @@ class MainTest:WordSpec({
         }
     })
     "part two" should ({
-        "QQQQJ handtype is five of a kind" {
-           Hand("QQQQJ").handTypeP2() shouldBe HandType.FiveOfAKind
+        "QQQQ* handtype is five of a kind" {
+           Hand("QQQQ*").handType() shouldBe HandType.FiveOfAKind
         }
         "QQQQ2 handtype is four of a kind" {
-           Hand("QQQQ2").handTypeP2() shouldBe HandType.FourOfAKind
+           Hand("QQQQ2").handType() shouldBe HandType.FourOfAKind
         }
-        "best hand type for 1234J is one pair" {
-            Hand("1234J").handTypeP2() shouldBe HandType.OnePair
+        "best hand type for 1234* is one pair" {
+            Hand("1234*").handType() shouldBe HandType.OnePair
         }
-        "best hand type for J234J is three of a kind" {
-            Hand("J234J").handTypeP2() shouldBe HandType.ThreeOfAKind
+        "best hand type for *234* is three of a kind" {
+            Hand("*234*").handType() shouldBe HandType.ThreeOfAKind
         }
-        "best hand type for 2234J is three of a kind" {
-            Hand("2234J").handTypeP2() shouldBe HandType.ThreeOfAKind
+        "best hand type for 2234* is three of a kind" {
+            Hand("2234*").handType() shouldBe HandType.ThreeOfAKind
         }
-        "best hand type for 2233J is full house" {
-            Hand("2233J").handTypeP2() shouldBe HandType.FullHouse
+        "best hand type for 2233* is full house" {
+            Hand("2233*").handType() shouldBe HandType.FullHouse
         }
-        "best hand type for 22JJ4 is four of a kind" {
-            Hand("22JJ4").handTypeP2() shouldBe HandType.FourOfAKind
+        "best hand type for 22**4 is four of a kind" {
+            Hand("22**4").handType() shouldBe HandType.FourOfAKind
         }
-        "best hand type for JJJ23 is four of a kind" {
-            Hand("JJJ23").handTypeP2() shouldBe HandType.FourOfAKind
+        "best hand type for ***23 is four of a kind" {
+            Hand("***23").handType() shouldBe HandType.FourOfAKind
         }
-        "best hand type for T55J5 is four of a kind" {
-            Hand("T55J5").handTypeP2() shouldBe HandType.FourOfAKind
+        "best hand type for T55*5 is four of a kind" {
+            Hand("T55*5").handType() shouldBe HandType.FourOfAKind
         }
-        "best hand type for 22JJ2 is five of a kind" {
-            Hand("22JJ2").handTypeP2() shouldBe HandType.FiveOfAKind
+        "best hand type for 22**2 is five of a kind" {
+            Hand("22**2").handType() shouldBe HandType.FiveOfAKind
         }
-        "best hand type for 2222J is five of a kind" {
-            Hand("2222J").handTypeP2() shouldBe HandType.FiveOfAKind
+        "best hand type for 2222* is five of a kind" {
+            Hand("2222*").handType() shouldBe HandType.FiveOfAKind
         }
-        "best hand type for JJJJ2 is five of a kind" {
-            Hand("JJJJ2").handTypeP2() shouldBe HandType.FiveOfAKind
+        "best hand type for ****2 is five of a kind" {
+            Hand("****2").handType() shouldBe HandType.FiveOfAKind
         }
-        "JKKK2 is weaker than QQQQ2" {
-            val hands = listOf("JKKK2 123", "QQQQ2 123").toHandsP2()
-            (hands[0].totalScoreP2() < hands[1].totalScoreP2()) shouldBe true
+        "*KKK2 is weaker than QQQQ2" {
+            val hands = listOf("*KKK2 123", "QQQQ2 123").toHandsP2()
+            (hands[0].totalScore() < hands[1].totalScore()) shouldBe true
         }
-        "JJJJJ is stronger than QQQQ2" {
-            val hands = listOf("JJJJJ 123", "QQQQ2 123").toHandsP2()
-            (hands[0].totalScoreP2() > hands[1].totalScoreP2()) shouldBe true
+        "***** is stronger than QQQQ2" {
+            val hands = listOf("***** 123", "QQQQ2 123").toHandsP2()
+            (hands[0].totalScore() > hands[1].totalScore()) shouldBe true
         }
         "testData winnings is 5905" {
             testData.toHandsP2().winningsP2() shouldBe 5905
