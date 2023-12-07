9516b95de0cb575cc3f7f3d74f161d4b90e57df1
Mike Neilens <mike.neilens@gmail.com>
2023-12-07 15:39:08 +0000

    day7 part two refactor

--- day07/src/main/kotlin/Main.kt
+++ day07/src/main/kotlin/Main.kt
@@ -8,19 +8,21 @@ fun main() {
 val cardValues:Map<Char, Int> =
     mapOf('A' to 13, 'K' to 12, 'Q' to 11, 'J' to 10, 'T' to 9, '9' to 8, '8' to 7, '7' to 6, '6' to 5, '5' to 4, '4' to 3, '3' to 2, '2' to 1, '*' to 0)
 
-enum class HandType(val value:Double) {
-    FiveOfAKind(6 * 14.0.pow(5)),
-    FourOfAKind(5 * 14.0.pow(5)),
-    FullHouse(4 * 14.0.pow(5)),
-    ThreeOfAKind(3 * 14.0.pow(5)),
-    TwoPair(2 * 14.0.pow(5)),
-    OnePair(14.0.pow(5)),
-    HighCard(0.0)
+enum class HandType(val value:Double, val typeWithWildCard:HandType?) {
+    FiveOfAKind(6 * 14.0.pow(5), FiveOfAKind),
+    FourOfAKind(5 * 14.0.pow(5), FiveOfAKind),
+    FullHouse(4 * 14.0.pow(5), FiveOfAKind),
+    ThreeOfAKind(3 * 14.0.pow(5), FourOfAKind),
+    TwoPair(2 * 14.0.pow(5), FullHouse),
+    OnePair(14.0.pow(5), ThreeOfAKind),
+    HighCard(0.0, OnePair);
+
+    fun promoteIf(hasWildCard:Boolean) = if (hasWildCard) this.typeWithWildCard else this
 }
 
 data class Hand(val cards:String, val bid:Int = 0) {
 
-    fun totalScore() = handType().value + cardsScore()
+    fun totalScore() = (handType()?.value ?: 0.0) + cardsScore()
 
     fun cardsScore() = cards.reversed().mapIndexed{n, c -> 14.0.pow(n) * cardValues.getValue(c)}.sum()
 
@@ -28,12 +30,12 @@ data class Hand(val cards:String, val bid:Int = 0) {
 
     fun handType() = when(noOfDistinctCards) {
         listOf(5) -> HandType.FiveOfAKind
-        listOf(1,4) -> if(hasAnyWildCards) HandType.FiveOfAKind else HandType.FourOfAKind
-        listOf(2,3) -> if(hasAnyWildCards) HandType.FiveOfAKind else HandType.FullHouse
-        listOf(1,1,3) -> if(hasAnyWildCards) HandType.FourOfAKind else HandType.ThreeOfAKind
-        listOf(1,2,2) -> if(hasTwoWildCards) HandType.FourOfAKind else if(hasAnyWildCards) HandType.FullHouse else HandType.TwoPair
-        listOf(1,1,1,2) -> if (hasAnyWildCards) HandType.ThreeOfAKind else HandType.OnePair
-        else -> if(hasAnyWildCards) HandType.OnePair else HandType.HighCard
+        listOf(1,4) -> HandType.FourOfAKind.promoteIf(hasAnyWildCards)// HandType.FiveOfAKind else HandType.FourOfAKind
+        listOf(2,3) -> HandType.FullHouse.promoteIf(hasAnyWildCards)
+        listOf(1,1,3) -> HandType.ThreeOfAKind.promoteIf(hasAnyWildCards)
+        listOf(1,2,2) -> if(hasTwoWildCards) HandType.FourOfAKind else HandType.TwoPair.promoteIf(hasAnyWildCards)
+        listOf(1,1,1,2) -> HandType.OnePair.promoteIf(hasAnyWildCards)
+        else ->  HandType.HighCard.promoteIf(hasAnyWildCards)
     }
 
     val hasAnyWildCards = cards.contains('*')
