ac4e715c678a76514b8c4ed9473736c459978c9c
Mike Neilens <mike.neilens@gmail.com>
2023-12-08 11:04:07 +0000

    day8

--- day08/src/main/kotlin/Main.kt
+++ day08/src/main/kotlin/Main.kt
@@ -1,11 +1,70 @@
-fun main(args: Array<String>) {
+
+import java.util.*
+
+
+fun main() {
     partOne(sampleData)
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+enum class Direction {
+    Left, Right
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+data class Instructions(var nextStep:Int = 0, val directions:List<Direction>, var count:Int = 0 ) {
+    fun nextStep():Direction {
+        val next = directions[nextStep]
+        if (nextStep < directions.lastIndex) nextStep ++ else nextStep = 0
+        count ++
+        return next
+    }
+}
+
+fun List<String>.toInstructions() = Instructions(directions  = first().toDirections())
+fun String.toDirections() = map {if (it == 'L') Direction.Left else Direction.Right}
+
+typealias Nodes = Map<String, Map<Direction, String>>
+
+fun Nodes.get(key:String, direction:Direction) = getValue(key).getValue(direction)
+
+fun List<String>.toNodes(nodes:MutableMap<String, Map<Direction, String>> = mutableMapOf()):Nodes {
+    drop(2).forEach { string ->
+        nodes[string.substring(0..2)] = mapOf(Direction.Left to string.substring(7..9), Direction.Right to string.substring(12..14)  )
+    }
+    return nodes
+}
+
+fun partOne(sampleData:List<String>) :Int {
+    val instructions = sampleData.toInstructions()
+    val nodes = sampleData.toNodes()
+    var key = "AAA"
+    while (key != "ZZZ") {
+        key = nodes.get(key, instructions.nextStep())
+    }
+    return instructions.count
+}
+
+fun partTwo(sampleData:List<String>):Long {
+    val nodes = sampleData.toNodes()
+    val nodesEndingInA= nodes.keys.filter{it.endsWith('A')}
+    val instructions = nodesEndingInA.map{sampleData.toInstructions()}
+    var keys = nodesEndingInA.toMutableList()
+    val factors = keys.mapIndexed { i, startKey ->
+        var key = startKey
+        while (!key.endsWith('Z')) {
+            key = nodes.get(key, instructions[i].nextStep())
+        }
+        instructions[i].count.toLong()
+    }
+    return lcm(factors)
+}
+
+private fun gcd(x: Long, y: Long): Long {
+    return if (y == 0L) x else gcd(y, x % y)
+}
+
+fun lcm(numbers: List<Long>): Long {
+    return numbers.fold(
+        1L
+    ) { x, y -> x * (y / gcd(x, y)) }
 }
--- day08/src/test/kotlin/MainTest.kt
+++ day08/src/test/kotlin/MainTest.kt
@@ -3,13 +3,74 @@ import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        val testData1 = """
+            RL
+
+            AAA = (BBB, CCC)
+            BBB = (DDD, EEE)
+            CCC = (ZZZ, GGG)
+            DDD = (DDD, DDD)
+            EEE = (EEE, EEE)
+            GGG = (GGG, GGG)
+            ZZZ = (ZZZ, ZZZ)
+        """.trimIndent().split("\n")
+
+        val testData2 = """
+            LLR
+
+            AAA = (BBB, BBB)
+            BBB = (AAA, ZZZ)
+            ZZZ = (ZZZ, ZZZ)
+        """.trimIndent().split("\n")
+
+        "for testData1 instructions has directions Right, Left" {
+            testData1.first().toDirections() shouldBe listOf(Direction.Right, Direction.Left)
+        }
+
+        "for testData1 first three instructions are Right, Left, Right" {
+            val instructions = testData1.toInstructions()
+            instructions.nextStep() shouldBe Direction.Right
+            instructions.nextStep() shouldBe Direction.Left
+            instructions.nextStep() shouldBe Direction.Right
+        }
+        "for testData1 first three nodes are AAA = (BBB, CCC), BBB = (DDD, EEE), CCC = (ZZZ, GGG)" {
+            val nodes = testData1.toNodes()
+            nodes.get("AAA",Direction.Left) shouldBe "BBB"
+            nodes.get("AAA",Direction.Right) shouldBe "CCC"
+            nodes.get("BBB",Direction.Left) shouldBe "DDD"
+            nodes.get("BBB",Direction.Right) shouldBe "EEE"
+            nodes.get("CCC",Direction.Left) shouldBe "ZZZ"
+            nodes.get("CCC",Direction.Right) shouldBe "GGG"
+        }
+
+        "part one with testData1 should be 2" {
+            partOne(testData1) shouldBe 2
+        }
+        "part one with testData2 should be 6" {
+            partOne(testData2) shouldBe 6
+        }
+        "part one with sampleData should be 13301" {
+            partOne(sampleData) shouldBe 13301
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        val testData2 = """
+            LR
+
+            11A = (11B, XXX)
+            11B = (XXX, 11Z)
+            11Z = (11B, XXX)
+            22A = (22B, XXX)
+            22B = (22C, 22C)
+            22C = (22Z, 22Z)
+            22Z = (22B, 22B)
+            XXX = (XXX, XXX)
+        """.trimIndent().split("\n")
+        "part two using testData2 should be 6" {
+            partTwo(testData2) shouldBe 6L
+        }
+        "part two using sample data should be 7309459565207" {
+            partTwo(sampleData) shouldBe 7309459565207L
         }
     })
 })
