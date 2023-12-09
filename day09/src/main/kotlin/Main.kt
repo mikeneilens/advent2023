cc5dc28a1e0406ba9d17f2a74f8749fe89942562
Mike Neilens <mike.neilens@gmail.com>
2023-12-09 11:50:18 +0000

    day9

--- day09/src/main/kotlin/Main.kt
+++ day09/src/main/kotlin/Main.kt
@@ -3,9 +3,26 @@ fun main(args: Array<String>) {
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun partOne(sampleData:List<String>) :Int {
+    return sampleData.parse().sumOf { it.sumLast() }
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+fun List<String>.parse() = map{it.split(" ").map(String::toInt)}
+
+fun List<Int>.toDifference():List<Int> = drop(1).fold(Pair(first(), listOf<Int>())){(last, result), v -> Pair(v, result + (v - last))}.second
+
+tailrec fun List<Int>.allDifferences(result:List<List<Int>> = mutableListOf() ):List<List<Int>> {
+    if (all { it==0 }) return result
+    else {
+        val differenceList:List<Int> = toDifference()
+        return differenceList.allDifferences(result + listOf(differenceList))
+    }
 }
+
+fun List<Int>.sumLast():Int = last() + allDifferences().sumOf{it.last()}
+
+fun partTwo(sampleData:List<String>):Int {
+    return sampleData.parse().sumOf { it.sumFirst() }
+}
+
+fun List<Int>.sumFirst():Int = first() - allDifferences().reversed().map{it.first()}.fold(0){a, v ->  v - a }
--- day09/src/test/kotlin/MainTest.kt
+++ day09/src/test/kotlin/MainTest.kt
@@ -2,14 +2,46 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+
+    val testData = """
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "parse test data" {
+            testData.parse() shouldBe listOf(listOf(0,3, 6, 9, 12, 15),listOf(1, 3, 6, 10, 15, 21),listOf(10, 13, 16, 21, 30, 45))
         }
+        "0 3 6 9 12 15 differences are listOf(3, 3, 3, 3, 3)" {
+            listOf(0, 3, 6, 9, 12, 15).toDifference() shouldBe  listOf(3, 3, 3, 3, 3)
+        }
+        "10 13 16 21 30 45 differences are listOf(3, 3, 3, 3, 3)" {
+            listOf(10, 13, 16, 21, 30, 45).toDifference() shouldBe  listOf(3, 3, 5, 9, 15)
+        }
+        "0 3 6 9 12 15 all differences are listOf(listOf(3, 3, 3, 3, 3),listOf(0, 0, 0, 0))" {
+            listOf(0, 3, 6, 9, 12, 15).allDifferences() shouldBe  listOf(listOf(3, 3, 3, 3, 3),listOf(0, 0, 0, 0))
+        }
+        "0 3 6 9 12 15 sum of differences is 18" {
+            listOf(0, 3, 6, 9, 12, 15).sumLast() shouldBe 18
+        }
+        "part one using testData should be 114" {
+            partOne(testData) shouldBe 114
+        }
+        "part one using sample data should be 2105961943" {
+            partOne(sampleData) shouldBe 2105961943
+        }
+
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        "10  13  16  21  30  45 sumFirst is 5" {
+            listOf(10,  13,  16,  21,  30,  45).sumFirst() shouldBe 5
+        }
+        "part two using test data should be 2" {
+             partTwo(testData) shouldBe 2
+        }
+        "part two using sample data should be 1019" {
+             partTwo(sampleData) shouldBe 1019
         }
     })
 })
