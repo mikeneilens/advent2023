e35228a3655077ffceaac440948334fe04b92cc2
Mike Neilens <mike.neilens@gmail.com>
2023-12-10 19:43:25 +0000

    day10

--- day10/src/main/kotlin/Main.kt
+++ day10/src/main/kotlin/Main.kt
@@ -1,11 +1,150 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+data class Position(val x:Int = 0, val y:Int = 0) {
+    infix operator fun plus(other:Position) = Position(this.x + other.x, this.y + other.y)
+
+    fun surroundPositions(maxX:Int, maxY:Int) = listOf(Position(x -1, y), Position( y -1, x),Position(x +1, y), Position( y +1, x)).filter{ it.x in 0..maxX && it.y in 0..it.y  }
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+data class ValidConnection(val pipes:List<Connection>)
+
+enum class Direction(val offset:Position){Left(Position(-1,0)), Right(Position(1,0)), Up(Position(0,-1)), Down(Position(0,1))}
+
+data class Connection(val type:Char, val direction:Direction)
+
+val validRightConnections = ValidConnection(pipes = listOf(Connection('-', Direction.Right), Connection('7', Direction.Right), Connection('J',Direction.Right),Connection('S',Direction.Right)))
+val validLeftConnections = ValidConnection(pipes = listOf(Connection('-', Direction.Left),Connection('F', Direction.Left),Connection('L', Direction.Left),Connection('S',Direction.Left)))
+val validUpConnections = ValidConnection(pipes = listOf(Connection('|', Direction.Up),Connection('7', Direction.Up),Connection('F', Direction.Up),Connection('S',Direction.Up)))
+val validDownConnections = ValidConnection(pipes = listOf(Connection('|',Direction.Down), Connection('J', Direction.Down),Connection('L', Direction.Down),Connection('S',Direction.Down)))
+
+val validConnections = mapOf(
+    '|' to ValidConnection(validUpConnections.pipes + validDownConnections.pipes),
+    '-' to ValidConnection(validLeftConnections.pipes + validRightConnections.pipes),
+    'L' to ValidConnection(validUpConnections.pipes + validRightConnections.pipes),
+    'J' to ValidConnection(validUpConnections.pipes + validLeftConnections.pipes),
+    '7' to ValidConnection(validLeftConnections.pipes + validDownConnections.pipes),
+    'F' to ValidConnection(validRightConnections.pipes + validDownConnections.pipes),
+    'S' to ValidConnection(validLeftConnections.pipes + validRightConnections.pipes + validUpConnections.pipes + validDownConnections.pipes)
+)
+
+typealias Maze = List<String>
+
+fun Maze.pipeAt(position:Position) = if (position.x !in 0..(first().lastIndex) || position.y !in 0..lastIndex) '.' else get(position.y)[position.x]
+
+fun Maze.pipesNextTo(position:Position) =
+    validConnections.getValue(pipeAt(position)).pipes
+        .filter{connection:Connection ->  connection.type == pipeAt(position + connection.direction.offset)}
+        .map{Pair(position + it.direction.offset, it)}
+
+fun Maze.pipesNotVisitedNextTo(position: Position, visited:MutableList<Position>) =
+    pipesNextTo(position).filter { it.first !in visited }
+
+tailrec fun Maze.traverseLoop(position:Position, visited:MutableList<Position> = mutableListOf(),  connections:MutableList<Connection> = mutableListOf()):MutableList<Position> {
+    visited.add(position)
+    val nextPipe = pipesNotVisitedNextTo(position,visited)
+    if (nextPipe.isEmpty()) return visited
+    else {
+        connections.add(nextPipe.first().second)
+        return traverseLoop(nextPipe.first().first,visited, connections)
+    }
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+fun Maze.startPosition():Position {
+    val startY = indexOfFirst { it.contains('S') }
+    return Position(this[startY].indexOfFirst { it == 'S'}, startY)
 }
+
+fun partOne(sampleData:List<String>,S:Char) :Int {
+    val startPosition = sampleData.startPosition()
+    val connections = mutableListOf<Connection>(Connection('S',Direction.Down))
+    val visited = sampleData.traverseLoop(startPosition, connections = connections)
+    return visited.size / 2
+}
+
+fun partTwo(sampleData:List<String>):Int {
+    val (visits, connections) = visitsAndConnections(sampleData)
+    val map = addVisitsToMap(visits)
+    map.SidesToMap(visits, connections, side1,'1')
+    map.SidesToMap(visits, connections, side2,'2')
+    map.floodMap('2', sampleData.first().lastIndex, sampleData.lastIndex)
+    map.print()
+    return map.values.count { it == '2' }
+}
+
+fun addVisitsToMap(visited:List<Position>):MutableMap<Position,Char> = visited.map{Pair(it, 'P')}.toMap().toMutableMap()
+
+fun MutableMap<Position,Char>.SidesToMap(visited: List<Position>, connections: List<Connection>, sideMap:Map<Connection,List<Position>>, symbol:Char) {
+    visited.zip(connections).forEach{(Position, Connection) ->
+        sideMap[Connection]?.forEach {offset ->
+            if (this[Position + offset] == null) this[Position + offset] = symbol
+        }
+    }
+}
+
+fun MutableMap<Position,Char>.floodMap(symbol:Char, maxX:Int, maxY:Int) {
+    val newPositions = filter{it.value == symbol}.keys.flatMap{it.surroundPositions(maxX, maxY)}.filter{this[it] == null}
+    if (newPositions.isEmpty()) return
+    newPositions.forEach { set(it, symbol) }
+    floodMap(symbol, maxX, maxY)
+}
+
+fun Map<Position, Char>.print() {
+    (0..(keys.map{it.y}.max())).forEach{y ->
+        (0..(keys.map{it.y}.max())).forEach { x ->
+            print( this[Position(x,y)] ?: ' ')
+        }
+        println()
+    }
+}
+
+fun visitsAndConnections(sampleData:List<String>):Pair<List<Position>,List<Connection>> {
+    val startPosition = sampleData.startPosition()
+    val connections = mutableListOf<Connection>(Connection('S',Direction.Down))
+    val visited = sampleData.traverseLoop(startPosition, connections = connections)
+    val directionOfS:Direction = sampleData.pipesNotVisitedNextTo(visited.last(),visited.drop(1).toMutableList()).first().second.direction
+    val typeOfS = sMap[Pair(connections.last().direction, connections[1].direction)] ?: ' '
+    connections[0] = Connection(typeOfS, directionOfS)
+    return Pair(visited, connections)
+}
+
+val sMap = mapOf(
+    Pair(Direction.Down,Direction.Right) to 'L',
+    Pair(Direction.Down,Direction.Down) to '|',
+    Pair(Direction.Down,Direction.Left) to 'J',
+    Pair(Direction.Left,Direction.Down) to 'F',
+    Pair(Direction.Left,Direction.Left) to '-',
+    Pair(Direction.Left,Direction.Up) to 'L',
+    Pair(Direction.Up,Direction.Left) to '7',
+    Pair(Direction.Up,Direction.Up) to '|',
+    Pair(Direction.Up,Direction.Right) to 'F',
+    Pair(Direction.Right,Direction.Up) to 'J',
+    Pair(Direction.Right,Direction.Right) to '-',
+    Pair(Direction.Right,Direction.Down) to '7',
+)
+
+val side1 = mapOf(
+    Connection('|', Direction.Up) to listOf(Position(x= -1)),
+    Connection('|', Direction.Down) to listOf(Position(x= +1)),
+    Connection('-', Direction.Right) to listOf(Position(y= -1)),
+    Connection('-', Direction.Left) to listOf(Position(y= +1)),
+    Connection('L', Direction.Left) to listOf(Position(x= -1),Position(x= -1,y= +1),Position(y= +1 )),
+    Connection('L', Direction.Down) to listOf(),
+    Connection('J', Direction.Down) to listOf(Position(x= +1),Position(x= +1,y= +1),Position(y= +1 )),
+    Connection('J', Direction.Right) to listOf(),
+    Connection('7', Direction.Right) to listOf(Position(x= +1),Position(x= +1,y= -1),Position(y= -1 )),
+    Connection('7', Direction.Up) to listOf(),
+    Connection('F', Direction.Up) to listOf(Position(x= -1),Position(x= -1,y= -1),Position(y= -1 )),
+    Connection('F', Direction.Left) to listOf(),
+)
+val side2 = mapOf(
+    Connection('|', Direction.Up) to side1.getValue(Connection('|', Direction.Down)),
+    Connection('|', Direction.Down) to side1.getValue(Connection('|', Direction.Up)),
+    Connection('-', Direction.Right) to side1.getValue(Connection('-', Direction.Left)),
+    Connection('-', Direction.Left) to side1.getValue(Connection('-', Direction.Right)),
+    Connection('L', Direction.Left) to side1.getValue(Connection('L', Direction.Down)),
+    Connection('L', Direction.Down) to  side1.getValue(Connection('L', Direction.Left)),
+    Connection('J', Direction.Down) to side1.getValue(Connection('J', Direction.Right)),
+    Connection('J', Direction.Right) to side1.getValue(Connection('J', Direction.Down)),
+    Connection('7', Direction.Right) to side1.getValue(Connection('7', Direction.Up)),
+    Connection('7', Direction.Up) to side1.getValue(Connection('7', Direction.Right)),
+    Connection('F', Direction.Up) to side1.getValue(Connection('F', Direction.Left)),
+    Connection('F', Direction.Left) to side1.getValue(Connection('F', Direction.Up)),
+)
--- day10/src/test/kotlin/MainTest.kt
+++ day10/src/test/kotlin/MainTest.kt
@@ -2,14 +2,87 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testData1 = """
+        .....
+        .S-7.
+        .|.|.
+        .L-J.
+        .....
+    """.trimIndent().split("\n")
+    val testData2 = """
+        ..F7.
+        .FJ|.
+        SJ.L7
+        |F--J
+        LJ...
+    """.trimIndent().split("\n")
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "pipe at 0,0 in testdata1 is '.'" {
+            testData1.pipeAt(Position(0,0)) shouldBe '.'
+        }
+        "pipe at 1,1 in testdata1 is 'S'" {
+            testData1.pipeAt(Position(1,1)) shouldBe 'S'
+        }
+        "pipe at -1,1 and 5,1 in testdata1 is '.'" {
+            testData1.pipeAt(Position(-1,1)) shouldBe '.'
+            testData1.pipeAt(Position(5,1)) shouldBe '.'
+        }
+        "pipe at 1,-1 and 1,5 in testdata1 is '.'" {
+            testData1.pipeAt(Position(1,-1)) shouldBe '.'
+            testData1.pipeAt(Position(1,5)) shouldBe '.'
+        }
+        "pipe locations next to 1,1 in test data are (2,1) and (1,2)  " {
+            testData1.pipesNextTo(Position(1,1)) shouldBe listOf(
+                Pair(Position(2,1),Connection('-',Direction.Right)),
+                Pair(Position(1,2),Connection('|',Direction.Down)))
+        }
+        "pipe locations not visited next to 1,1 when (1,2) has been visited in test data are (2,1) " {
+            testData1.pipesNotVisitedNextTo(Position(1,1), mutableListOf(Position(1,2))) shouldBe listOf(Pair(Position(x=2, y=1), Connection('-', Direction.Right)))
+        }
+        "pipe locations not visited next to 1,1 when (1,2) and (2,1) has been visited in test data is empty " {
+            testData1.pipesNotVisitedNextTo(Position(1,1), mutableListOf(Position(1,2),Position(2,1))) shouldBe listOf<Pair<Int,Int>>()
+        }
+        "traversing loop of testdata1 starting at 1,1" {
+            val connections = mutableListOf<Connection>()
+            testData1.traverseLoop(Position(1,1), connections= connections) shouldBe listOf(
+                Position(x=1, y=1),
+                Position(x=2, y=1),
+                Position(x=3, y=1),
+                Position(x=3, y=2),
+                Position(x=3, y=3),
+                Position(x=2, y=3),
+                Position(x=1, y=3),
+                Position(x=1, y=2))
+            connections shouldBe listOf(
+                Connection(type='-', direction=Direction.Right),
+                Connection(type='7', direction=Direction.Right),
+                Connection(type='|', direction=Direction.Down),
+                Connection(type='J', direction=Direction.Down),
+                Connection(type='-', direction=Direction.Left),
+                Connection(type='L', direction=Direction.Left),
+                Connection(type='|', direction=Direction.Up)
+            )
+        }
+        "traversing loop of testdata2 starting at 0,2" {
+            testData2.traverseLoop(Position(0,2)) shouldBe listOf(
+                Position(x=0, y=2), Position(x=1, y=2), Position(x=1, y=1), Position(x=2, y=1),
+                Position(x=2, y=0), Position(x=3, y=0), Position(x=3, y=1), Position(x=3, y=2),
+                Position(x=4, y=2), Position(x=4, y=3), Position(x=3, y=3), Position(x=2, y=3),
+                Position(x=1, y=3), Position(x=1, y=4), Position(x=0, y=4), Position(x=0, y=3))
+        }
+        "part one using testData1 should be 4" {
+            partOne(testData1,'F') shouldBe 4
+        }
+        "part one using testData2 should be 8" {
+            partOne(testData2, 'F') shouldBe 8
+        }
+        "part one using sample data should be 7005" {
+            partOne(sampleData, '7') shouldBe 7005
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        "part two with sampleData is 417" {
+            partTwo(sampleData) shouldBe 417
         }
     })
 })
