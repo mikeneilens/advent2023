955ae8b4329756a33ea985e45be2642afa35b976
Mike Neilens <mike.neilens@gmail.com>
2023-12-11 10:52:25 +0000

    day 11 part one

--- day11/src/main/kotlin/Main.kt
+++ day11/src/main/kotlin/Main.kt
@@ -3,9 +3,38 @@ fun main(args: Array<String>) {
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+data class Position(val x:Int = 0, val y:Int = 0) {
+    infix operator fun plus(other:Position) = Position(this.x + other.x, this.y + other.y)
 }
-fun partTwo(sampleData:String):Int {
+
+fun partOne(sampleData:List<String>) :Int {
+    val rowWidths = sampleData.rowWidths()
+    val colWidths = sampleData.colWidths()
+    return sampleData.toPositions().total(rowWidths, colWidths)
+}
+
+fun List<String>.toPositions() = (0..(first().lastIndex)).flatMap { x ->
+    (0..lastIndex).mapNotNull{y -> if (this[y][x] == '#') Position(x,y) else null}}
+
+fun List<Position>.total(rowWidths:Map<Int, Int>, colWidths:Map<Int, Int>) =
+    flatMap{first -> map{second -> Pair(first, second) }}.sumOf{it.first.distanceTo(it.second, rowWidths, colWidths)}/2
+
+fun List<String>.rowWidths() =
+    mapIndexed { i, s ->
+        if (s.any { it == '#' }) Pair(i,1) else Pair(i,2)
+    }.toMap()
+
+fun List<String>.colWidths() =
+    first().mapIndexed { i, _ ->
+        if (this.map{it[i]}.any{ it == '#'}) Pair(i,1) else Pair(i,2)
+    }.toMap()
+
+fun distanceTo(s:Int, e:Int, colWidths:Map<Int, Int>) =
+    ((minOf(s, e) +1)..(maxOf(s, e) -1)).sumOf{ colWidths.getValue(it)} + 1
+
+fun Position.distanceTo(other:Position, rowWidths:Map<Int, Int>, colWidths:Map<Int, Int>) =
+    distanceTo(x, other.x, colWidths) + distanceTo(y, other.y, rowWidths) - listOf(Pair(x, other.x), Pair(y, other.y)).map{ if (it.first == it.second) 1 else 0 }.sum()
+
+fun partTwo(sampleData:List<String>):Int {
     return 0
 }
--- day11/src/test/kotlin/MainTest.kt
+++ day11/src/test/kotlin/MainTest.kt
@@ -2,9 +2,73 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+
+    val testData1 = """
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "col widths" {
+            val colWidths = testData1.colWidths()
+            colWidths[0] shouldBe 1
+            colWidths[1] shouldBe 1
+            colWidths[2] shouldBe 2
+            colWidths[3] shouldBe 1
+            colWidths[4] shouldBe 1
+            colWidths[5] shouldBe 2
+        }
+        "row widths" {
+            val rowWidths = testData1.rowWidths()
+            rowWidths[0] shouldBe 1
+            rowWidths[1] shouldBe 1
+            rowWidths[2] shouldBe 1
+            rowWidths[3] shouldBe 2
+        }
+        "distance to row = 0 and row = 8 is 10" {
+            val rowWidths = testData1.rowWidths()
+            distanceTo(0,8, rowWidths) shouldBe 10
+        }
+        "distance to col = 3 and col = 7 is 5" {
+            val colWidths = testData1.colWidths()
+            distanceTo(3,7, colWidths) shouldBe 5
+        }
+        "distance between row = 0, col= 3 and row = 8, col =7 is 15" {
+            val rowWidths = testData1.rowWidths()
+            val colWidths = testData1.colWidths()
+            Position(3,0).distanceTo(Position(7,8), rowWidths, colWidths) shouldBe 15
+        }
+        "distance between row = 2, col= 0 and row = 6, col =9 is 17" {
+            val rowWidths = testData1.rowWidths()
+            val colWidths = testData1.colWidths()
+            Position(0,2).distanceTo(Position(9,6), rowWidths, colWidths) shouldBe 17
+        }
+        "distance between row = 9, col= 0 and row = 9, col =4 is 5" {
+            val rowWidths = testData1.rowWidths()
+            val colWidths = testData1.colWidths()
+            Position(0,9).distanceTo(Position(4,9), rowWidths, colWidths) shouldBe 5
+        }
+        "distance between row = 5, col= 5 and row = 5, col =5 is 0" {
+            val rowWidths = testData1.rowWidths()
+            val colWidths = testData1.colWidths()
+            Position(5,5).distanceTo(Position(5,5), rowWidths, colWidths) shouldBe 0
+        }
+        "testdata1 to positions " {
+            testData1.toPositions() shouldBe listOf(Position(x=0, y=2), Position(x=0, y=9), Position(x=1, y=5), Position(x=3, y=0), Position(x=4, y=9), Position(x=6, y=4), Position(x=7, y=1), Position(x=7, y=8), Position(x=9, y=6))
+        }
+        "part one for testData should be 374" {
+            partOne(testData1) shouldBe 374
+        }
+        "part one for sample data should be 374" {
+            partOne(sampleData) shouldBe 374
         }
     })
     "part two" should ({
