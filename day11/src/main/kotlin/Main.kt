1da82102fc845b3228293cbd57c3ef2306e6bc8f
Mike Neilens <mike.neilens@gmail.com>
2023-12-11 19:23:17 +0000

    day 11

--- day11/src/main/kotlin/Main.kt
+++ day11/src/main/kotlin/Main.kt
@@ -3,23 +3,23 @@ data class Position(val x:Int = 0, val y:Int = 0)
 fun partOne(sampleData:List<String>, largeWidth:Int = 2) :Long {
     val rowWidths = sampleData.rowWidths(largeWidth)
     val colWidths = sampleData.colWidths(largeWidth)
-    return sampleData.toPositions().total(rowWidths, colWidths)
+    val distanceCalculator = { pair:Set<Position> -> pair.first().distanceTo(pair.last(), rowWidths, colWidths).toLong()}
+    return sampleData.toPositions().total(distanceCalculator)
 }
 
 fun List<String>.toPositions() = (0..(first().lastIndex)).flatMap { x ->
     (0..lastIndex).mapNotNull{y -> if (this[y][x] == '#') Position(x,y) else null}}
 
-fun List<Position>.total(rowWidths:Map<Int, Int>, colWidths:Map<Int, Int>) =
-    galaxyPairs().sumOf{it.first().distanceTo(it.last(), rowWidths, colWidths).toLong()}
+fun List<Position>.total(distanceBetween:(Set<Position>)->Long) = galaxyPairs().sumOf(distanceBetween)
 
 tailrec fun List<Position>.galaxyPairs(result:List<Set<Position>> = listOf()):List<Set<Position>> =
     if (size < 2) result else drop(1).galaxyPairs( result + drop(1).map{setOf(first(),it) })
 
 fun List<String>.rowWidths(largeWidth:Int = 2) =
-    mapIndexed { i, s -> if ('#' in s) Pair(i,1) else Pair(i,largeWidth) }.toMap()
+    indices.associate { i -> if ('#' in get(i)) Pair(i,1) else Pair(i,largeWidth) }
 
 fun List<String>.colWidths(largeWidth:Int = 2) =
-    first().indices.map {i -> if ('#' in this.map{it[i]}) Pair(i,1) else Pair(i,largeWidth) }.toMap()
+    first().indices.associate { i -> if ('#' in this.map { it[i] }) Pair(i, 1) else Pair(i, largeWidth) }
 
 fun Position.distanceTo(other:Position, rowWidths:Map<Int, Int>, colWidths:Map<Int, Int>) =
     distanceTo(x, other.x, colWidths) + distanceTo(y, other.y, rowWidths) - noOfMatchingCoordinates(other)
