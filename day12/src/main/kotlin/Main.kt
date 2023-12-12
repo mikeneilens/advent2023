410474d19145d148eec3daf22ef34011264ab68f
Mike Neilens <mike.neilens@gmail.com>
2023-12-12 13:01:50 +0000

    day12

--- day12/src/main/kotlin/Main.kt
+++ day12/src/main/kotlin/Main.kt
@@ -1,11 +1,49 @@
+import kotlin.math.pow
+
 fun main(args: Array<String>) {
     partOne(sampleData)
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun String.matches(patternMapping:List<Int>) =
+    fold(listOf(mutableListOf<Char>())){ result, char ->
+        if (result.last().isEmpty() || result.last().last() != char) result + listOf(mutableListOf(char))
+        else {
+            result.last().add(char)
+            result}
+    }.filter{it.contains('#')}.map{it.size} == patternMapping
+
+fun String.qPositions() = mapIndexedNotNull { index, c -> if (c == '?') index else null }
+
+fun String.replaceQMarks(qPositions:List<Int>, binaryPattern:String) =
+    mapIndexed { index, c ->
+        val indexOfQ = qPositions.indexOfFirst{it == index}
+        if (indexOfQ >= 0 ) binaryPattern[indexOfQ] else c
+    }.joinToString("")
+
+fun Int.toPattern(length:Int) = ("0".repeat(length) + toString(2))
+    .takeLast(length)
+    .replace('1','#')
+    .replace('0','.')
+
+fun binaryPatterns(qPositions:List<Int>, missingSprings:Int):List<String> =
+    (0..(2.0.pow(qPositions.size).toInt())).map{ it.toPattern(qPositions.size)}.dropLast(1).filter{it.count{it == '#'} == missingSprings}
+
+fun String.variants(missingSprings: Int):List<String> {
+    val qPositions = qPositions()
+    val binaryPatterns = binaryPatterns(qPositions, missingSprings)
+    return binaryPatterns.map{ this.replaceQMarks(qPositions, it) }
 }
-fun partTwo(sampleData:String):Int {
+
+data class Springs(val status:String, val groups:List<Int>) {
+    val missingSprings = groups.sum() - status.count { it == '#' }
+}
+
+fun String.toSprings()= Springs(  split(" ").first(), split(" ").last().split(",").map(String::toInt))
+
+fun partOne(sampleData:List<String>) :Int {
+    return sampleData.map(String::toSprings).sumOf{springs -> springs.status.variants(springs.missingSprings).count{it.matches(springs.groups)}}
+}
+fun partTwo(sampleData:List<String>):Int {
     return 0
 }
--- day12/src/test/kotlin/MainTest.kt
+++ day12/src/test/kotlin/MainTest.kt
@@ -1,10 +1,57 @@
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testData = """
+        ???.### 1,1,3
+        .??..??...?##. 1,1,3
+        ?#?#?#?#?#?#?#? 1,3,1,6
+        ????.#...#... 4,1,1
+        ????.######..#####. 1,6,5
+        ?###???????? 3,2,1
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "#.#.### has pattern 1,1,3" {
+            "#.#.###".matches(listOf(1,1,3)) shouldBe true
+        }
+        ".###.##....# has pattern 3,2,1" {
+            ".###.##....#".matches(listOf(3,2,1)) shouldBe true
+        }
+        "missing springs in .??..??...?##. 1,1,3 is  3" {
+            val springs = ".??..??...?##. 1,1,3".toSprings()
+            springs.missingSprings shouldBe 3
+        }
+        "positions of question marks in .??..??...?##. are 1,2,5,6,10 " {
+            ".??..??...?##.".qPositions() shouldBe listOf(1,2,5,6,10)
+        }
+        "12 as a pattern with length 8 is " {
+            12.toPattern(8) shouldBe "....##.."
+        }
+        "replace q marks in .??..??...?##. with #.##. gives .#...##....## " {
+            val qPositions = ".??..??...?##.".qPositions()
+            val pattern = "#.##."
+                ".??..??...?##.".replaceQMarks(qPositions, pattern) shouldBe ".#...##....##."
+        }
+        "binary patterns for .??....?##. are '...', '..#', '.#.', '.##', '#..', '#.#', '##.', '###' " {
+            val qPositions = ".??....?##.".qPositions()
+            binaryPatterns(qPositions,2) shouldBe listOf(".##", "#.#","##.")
+        }
+        "variants of .??....?##. are ['........##', '.......###', '..#.....##', '..#....###', '.#......##', '.#.....###', '.##.....##', '.##....###']" {
+            ".??....?##".variants(2) shouldBe listOf("..#....###", ".#.....###", ".##.....##")
+        }
+        "valid variants of .??..??...?##. [1,1,3] should be 4 " {
+            ".??..??...?##.".variants(3).count{it.matches(listOf(1,1,3))} shouldBe 4
+        }
+        "valid variants of ?###???????? listOf(3,2,1) should be 10 " {
+            "?###????????".variants(3).count{it.matches(listOf(3,2,1))} shouldBe 10
+        }
+        "part one using testdata be 21" {
+            partOne(testData) shouldBe 21
+        }
+        "part one should be 6827" {
+            partOne(sampleData) shouldBe 6827
         }
     })
     "part two" should ({
