63dbbe54fd15fd5532bd138da436c82c58772596
Mike Neilens <mike.neilens@gmail.com>
2023-12-13 18:55:49 +0000

    day 13

--- day13/src/main/kotlin/Main.kt
+++ day13/src/main/kotlin/Main.kt
@@ -3,9 +3,55 @@ fun main(args: Array<String>) {
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+
+fun getRanges(n:Int, max:Int) =
+    if (n <= max/2) Pair(0..n, (n + 1) ..(2*n + 1))
+    else Pair( (n - (max + 1 - n) )..n - 1   ,n .. max)
+
+fun List<String>.toMirrors() = joinToString("\n").split("\n\n").map{it.split("\n")}
+
+fun List<String>.verticalRanges() =
+    (0..lastIndex).map{i -> getRanges(i, lastIndex)}.filter{it.second.last <= lastIndex}
+
+
+fun List<String>.mirrorOnRow(filter:List<Int> = listOf(-1)):Int {
+    val ranges = verticalRanges()
+    val matchingRow =  ranges.mapIndexed {i, (r1,r2) -> if (r1.zip(r2.reversed()).all{this[it.first] == this[it.second]}) i else -1}.filter{it !in filter}.maxOrNull()
+    return if (matchingRow!= null) ranges[matchingRow].first.last + 1 else 0
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+fun List<String>.transpose() =  first().indices.map{i -> map{ it[i]}.joinToString("") }
+
+fun List<String>.mirrorOnColumn(filter:List<Int> = listOf(-1)):Int {
+    return transpose().mirrorOnRow(filter)
 }
+
+fun partOne(sampleData:List<String>) :Int {
+    return sampleData.toMirrors().map{Pair(it.mirrorOnRow(),it.mirrorOnColumn())}.sumOf { it.first *100 + it.second }
+}
+fun partTwo(sampleData:List<String>):Int {
+    return sampleData.toMirrors().map{it.alternateMirror()}.sumOf { it.first *100 + it.second }
+}
+
+fun List<String>.alternateMirror():Pair<Int, Int> {
+    val currentMirrorOnRow = mirrorOnRow()
+    val currentMirrorOnCol = mirrorOnColumn()
+    (0..(first().lastIndex)).forEach{ x->
+        (0..lastIndex).forEach{ y ->
+            val updatedWindow = replace(x,y)
+            updatedWindow.mirrorOnLine(currentMirrorOnRow,List<String>::mirrorOnRow)?.let { return Pair(it,0) }
+            updatedWindow.mirrorOnLine(currentMirrorOnCol,List<String>::mirrorOnColumn)?.let { return Pair(0,it) }
+        }
+    }
+    return Pair(0,0 )
+}
+
+fun List<String>.mirrorOnLine(current:Int, mirrorOnRowOrColumn:List<String>.(List<Int>)->Int ):Int? {
+    val mirrorOnLine = mirrorOnRowOrColumn(listOf(-1, current - 1 ))
+    if (mirrorOnLine > 0 && mirrorOnLine != current) return mirrorOnLine else return null
+}
+
+fun List<String>.replace(x:Int, y:Int) =
+    mapIndexed{row, string -> string.mapIndexed{col, c-> if (row == y && col == x) c.swapChar() else c }.joinToString("")  }
+
+fun Char.swapChar() = if (this == '.') '#' else '.'
--- day13/src/test/kotlin/MainTest.kt
+++ day13/src/test/kotlin/MainTest.kt
@@ -2,14 +2,154 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+
+    val testdata1 = """
+            #.##..##.
+            ..#.##.#.
+            ##......#
+            ##......#
+            ..#.##.#.
+            ..##..##.
+            #.#.##.#.
+
+            #...##..#
+            #....#..#
+            ..##..###
+            #####.##.
+            #####.##.
+            ..##..###
+            #....#..#
+        """.trimIndent().split("\n")
+
+    val testdata2 = """
+    ##..####..##..#
+    #.##.#..##..##.
+    ##..###.##..##.
+    .#..#.#........
+    ######.########
+    ##..##...####..
+    ##..###.#....#.
+    .####.#.######.
+    #.##.#..##..##.
+    #....#..##..##.
+    #....##.##..##.
+    ######.##.##.##
+    .####..###..###
+    """.trimIndent().split("\n")
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+
+        "ranges for position 0 in a list containing 6 is " {
+            val (r1, r2) = getRanges(0,5)
+            r1 shouldBe 0..0
+            r2 shouldBe 1..1
+        }
+        "ranges for position 1 in a list containing 6 is " {
+            val (r1, r2) = getRanges(1,5)
+            r1 shouldBe 0..1
+            r2 shouldBe 2..3
+        }
+        "ranges for position 2 in a list containing 6 is " {
+            val (r1, r2) = getRanges(2,5)
+            r1 shouldBe 0..2
+            r2 shouldBe 3..5
+        }
+        "ranges for position 3 in a list containing 6 is " {
+            val (r1, r2) = getRanges(3,5)
+            r1 shouldBe 0..2
+            r2 shouldBe 3..5
+        }
+        "ranges for position 4 in a list containing 6 is " {
+            val (r1, r2) = getRanges(4,5)
+            r1 shouldBe 2..3
+            r2 shouldBe 4..5
+        }
+        "ranges for position 5 in a list containing 6 is " {
+            val (r1, r2) = getRanges(5,5)
+            r1 shouldBe 4..4
+            r2 shouldBe 5..5
+        }
+        "ranges for position 1 in a list containing 7 is " {
+            val (r1, r2) = getRanges(1,6)
+            r1 shouldBe 0..1
+            r2 shouldBe 2..3
+        }
+        "ranges for position 2 in a list containing 7 is " {
+            val (r1, r2) = getRanges(2,6)
+            r1 shouldBe 0..2
+            r2 shouldBe 3..5
+        }
+        "ranges for position 3 in a list containing 7 is " {
+            val (r1, r2) = getRanges(3,6)
+            r1 shouldBe 0..3
+            r2 shouldBe 4..7
+        }
+        "ranges for position 4 in a list containing 7 is " {
+            val (r1, r2) = getRanges(4,6)
+            r1 shouldBe 1..3
+            r2 shouldBe 4..6
+        }
+        "ranges for position 5 in a list containing 7 is " {
+            val (r1, r2) = getRanges(5,6)
+            r1 shouldBe 3..4
+            r2 shouldBe 5..6
+        }
+        "test data splits into two mirrors" {
+            val mirrors = testdata1.toMirrors()
+            mirrors.size shouldBe 2
+            mirrors[0].size shouldBe 7
+            mirrors[1].size shouldBe 7
+        }
+        "vertical ranges for first mirror in test data" {
+            val mirror = testdata1.toMirrors().first()
+            mirror.verticalRanges() shouldBe listOf(
+                Pair(0..0, 1..1),
+                Pair(0..1, 2..3),
+                Pair(0..2, 3..5),
+                Pair(1..3, 4..6),
+                Pair(3..4, 5..6),
+                Pair(5..5, 6..6))
+        }
+
+        "transpose [abcd],[efgh] is [ae,bf,cg,dh]" {
+            listOf("abcd","efgh").transpose() shouldBe listOf("ae","bf","cg","dh")
+        }
+
+        "mirrored col on 1st test data" {
+            val mirror = testdata1.toMirrors()[0]
+            mirror.mirrorOnColumn() shouldBe 5
+        }
+
+        "mirrored row in 2nd test data" {
+            val mirror = testdata1.toMirrors()[1]
+            mirror.mirrorOnRow() shouldBe 4
+        }
+
+        "part one with test data should be 405" {
+            partOne(testdata1) shouldBe 405
+        }
+        "part one with sample data should be 0" {
+            partOne(sampleData) shouldBe 33780
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        "replace 0,0 in test data " {
+            testdata1.replace(0,0)[0][0] shouldBe '.'
+        }
+        "replace 1,0 in test data " {
+            testdata1.replace(1,0)[0][1] shouldBe '#'
+        }
+        "alternate mirror for first mirror on testdata1" {
+            testdata1.toMirrors()[0].alternateMirror() shouldBe Pair(3,0)
+        }
+        "alternate mirror for 2nd mirror on testdata1" {
+            testdata1.toMirrors()[1].alternateMirror() shouldBe Pair(1,0)
+        }
+        "part two with testdata1 should be 400" {
+            partTwo(testdata1) shouldBe 400
+        }
+        "part two with sample data should be 400" {
+            partTwo(sampleData) shouldBe 23479
         }
     })
 })
+
