59a1793b22fc7647cf0019b814a516b597b60314
Mike Neilens <mike.neilens@gmail.com>
2023-12-14 17:32:05 +0000

    day12

--- day14/src/main/kotlin/Main.kt
+++ day14/src/main/kotlin/Main.kt
@@ -3,9 +3,66 @@ fun main(args: Array<String>) {
     partTwo(sampleData)
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun partOne(sampleData:List<String>) :Int {
+    return sampleData.weightOnEachRow(sampleData.rollsNorth()).sum()
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+fun partTwo(sampleData:List<String>):Int {
+    val scores = (0..300).fold(listOf(Pair(sampleData,0))){ a, v ->
+        val cycled = a.last().first.cycled()
+        a + listOf(Pair(cycled, cycled.weight()))
+    }.map{it.second}
+    val mapOfScoreIndices:MutableMap<Int, List<Int>> = mutableMapOf()
+    scores.indices.forEach { i -> mapOfScoreIndices[scores[i]] = (mapOfScoreIndices[scores[i]] ?: listOf()) + listOf(i)}
+    val firstRepeat = mapOfScoreIndices.toList().filter{it.second.size > 1}
+    return scores[ firstRepeat.first().second[0] + (1000000000 - firstRepeat.first().second[0]) % (firstRepeat.first().second[1] - firstRepeat.first().second[0])]
 }
+
+fun String.rollsNorth(lineAbove:String, rollsAbove:List<Int>) =
+    mapIndexed {i, c -> if (c == '#') 0 else if (lineAbove[i] == '.' ) rollsAbove[i] + 1 else rollsAbove[i]}
+
+fun List<String>.rollsNorth() =
+    fold(Pair( "#".repeat(size), listOf("0".repeat(size).map{"$it".toInt()}))){
+        (lineAbove, rollsAbove),string ->
+        Pair(string, rollsAbove + listOf( string.rollsNorth(lineAbove, rollsAbove.last())))
+    }.second.drop(1)
+
+fun String.rollsWest() =
+    foldIndexed(listOf<Int>()){i, result, c -> if (i == 0 || c == '#') result + listOf(0) else if (this[i -1] == '.' ) result + listOf(result.last() + 1) else result + listOf(result.last())}
+
+fun List<String>.rollsWest() = map(String::rollsWest)
+
+fun List<String>.mappedNorth():List<String> {
+    val rollsNorth = rollsNorth()
+    val map:MutableMap<Pair<Int,Int>, Char> = mutableMapOf()
+    forEachIndexed {row, string ->
+        string.forEachIndexed{col, c ->
+            if (c != '.') map[Pair(row - rollsNorth[row][col], col)] = c
+        }
+    }
+    return mapIndexed {row, string -> string.indices.map{col -> map[Pair(row, col)] ?: '.'   }.joinToString("")}
+}
+
+fun List<String>.mappedWest():List<String> {
+    val rollsWest = rollsWest()
+    val map:MutableMap<Pair<Int,Int>, Char> = mutableMapOf()
+    forEachIndexed {row, string ->
+        string.forEachIndexed{col, c ->
+            if (c != '.') map[Pair(row, col - rollsWest[row][col])] = c
+        }
+    }
+    return mapIndexed {row, string -> string.indices.map{col -> map[Pair(row, col)] ?: '.'   }.joinToString("")}
+}
+
+fun List<String>.cycled():List<String> =
+    mappedNorth().mappedWest().reversed().mappedNorth().reversed().map{it.reversed()}.mappedWest().map { it.reversed() }
+
+
+fun List<String>.weightOnEachRow(rollsAbove:List<List<Int>>) =
+    mapIndexed{ row, string ->
+        string.mapIndexed{col, c ->
+            if (c == 'O') size - (row -  rollsAbove[row][col]) else 0
+        }.sum()
+    }
+
+fun List<String>.weight() = mapIndexed {i, s -> (size - i) * s.count { it == 'O' } }.sum()
+
--- day14/src/test/kotlin/MainTest.kt
+++ day14/src/test/kotlin/MainTest.kt
@@ -2,14 +2,116 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testdata1 = """
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....
+    """.trimIndent().split("\n")
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "calculate rolls aboove a line" {
+            val lineAbove = "O.OO#....#"
+            val string    = ".....##..."
+            val rollsAbove = listOf(0,1,1,1,0,0,1,1,1,0)
+            string.rollsNorth(lineAbove, rollsAbove) shouldBe listOf(0,2,1,1,0,0,0,2,2,0)
+        }
+        "calculate rolls north for the test data" {
+            testdata1.rollsNorth() shouldBe listOf(
+                listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                listOf(0, 1, 1, 1, 0, 0, 1, 1, 1, 0),
+                listOf(0, 2, 1, 1, 0, 0, 0, 2, 2, 0),
+                listOf(1, 3, 2, 0, 1, 0, 0, 3, 3, 1),
+                listOf(1, 3, 3, 0, 1, 1, 1, 4, 0, 1),
+                listOf(2, 3, 0, 1, 2, 2, 2, 0, 0, 0),
+                listOf(2, 4, 0, 2, 3, 0, 3, 0, 1, 0),
+                listOf(3, 5, 0, 3, 4, 0, 3, 1, 2, 0),
+                listOf(0, 6, 1, 4, 5, 0, 0, 0, 3, 1),
+                listOf(0, 7, 2, 5, 6, 0, 0, 0, 4, 2)
+            )
+        }
+        "weights on each row in test data" {
+            val rollsAbove = testdata1.rollsNorth()
+            testdata1.weightOnEachRow(rollsAbove) shouldBe listOf(10, 29, 0, 34, 19, 14, 15, 4, 0, 11)
+        }
+        "part one with test data should be 136" {
+            partOne(testdata1) shouldBe 136
+        }
+        "part one with sample data should be 105208" {
+            partOne(sampleData) shouldBe 105208
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        "OO.#O....O rolled west should be" {
+            "OO.#O....O".rollsWest() shouldBe listOf(0, 0, 0, 0, 0, 0, 1, 2, 3, 4)
         }
+        "test data rolled west" {
+            testdata1.mappedWest() shouldBe listOf(
+                "O....#....",
+                "OOO.#....#",
+                ".....##...",
+                "OO.#OO....",
+                "OO......#.",
+                "O.#O...#.#",
+                "O....#OO..",
+                "O.........",
+                "#....###..",
+                "#OO..#...."
+            )
+        }
+        "sample data cycled once" {
+            testdata1.cycled() shouldBe """
+                .....#....
+                ....#...O#
+                ...OO##...
+                .OO#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#....
+                ......OOOO
+                #...O###..
+                #..OO#....
+            """.trimIndent().split("\n")
+        }
+        "sample data cycled twice" {
+            testdata1.cycled().cycled() shouldBe """
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #..OO###..
+                #.OOO#...O
+            """.trimIndent().split("\n")
+        }
+        "sample data cycled three" {
+            testdata1.cycled().cycled().cycled() shouldBe """
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #...O###.O
+                #.OOO#...O
+            """.trimIndent().split("\n")
+        }
+        "part two with testdata should be 0" {
+           // partTwo(testdata1) shouldBe 0
+        }
+        "part two with sampledata should be 0" {
+            partTwo(sampleData) shouldBe 102943
+        }
+
     })
 })
