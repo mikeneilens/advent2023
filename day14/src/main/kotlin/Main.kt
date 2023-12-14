18380859d94a4897176d68b564c64f2cefc80d04
Mike Neilens <mike.neilens@gmail.com>
2023-12-14 17:35:49 +0000

    day14

--- day14/src/main/kotlin/Main.kt
+++ day14/src/main/kotlin/Main.kt
@@ -6,6 +6,23 @@ fun main(args: Array<String>) {
 fun partOne(sampleData:List<String>) :Int {
     return sampleData.weightOnEachRow(sampleData.rollsNorth()).sum()
 }
+
+fun List<String>.weightOnEachRow(rollsAbove:List<List<Int>>) =
+    mapIndexed{ row, string ->
+        string.mapIndexed{col, c ->
+            if (c == 'O') size - (row -  rollsAbove[row][col]) else 0
+        }.sum()
+    }
+
+fun String.rollsNorth(lineAbove:String, rollsAbove:List<Int>) =
+    mapIndexed {i, c -> if (c == '#') 0 else if (lineAbove[i] == '.' ) rollsAbove[i] + 1 else rollsAbove[i]}
+
+fun List<String>.rollsNorth() =
+    fold(Pair( "#".repeat(size), listOf("0".repeat(size).map{"$it".toInt()}))){
+        (lineAbove, rollsAbove),string ->
+        Pair(string, rollsAbove + listOf( string.rollsNorth(lineAbove, rollsAbove.last())))
+    }.second.drop(1)
+
 fun partTwo(sampleData:List<String>):Int {
     val scores = (0..300).fold(listOf(Pair(sampleData,0))){ a, v ->
         val cycled = a.last().first.cycled()
@@ -17,30 +34,10 @@ fun partTwo(sampleData:List<String>):Int {
     return scores[ firstRepeat.first().second[0] + (1000000000 - firstRepeat.first().second[0]) % (firstRepeat.first().second[1] - firstRepeat.first().second[0])]
 }
 
-fun String.rollsNorth(lineAbove:String, rollsAbove:List<Int>) =
-    mapIndexed {i, c -> if (c == '#') 0 else if (lineAbove[i] == '.' ) rollsAbove[i] + 1 else rollsAbove[i]}
-
-fun List<String>.rollsNorth() =
-    fold(Pair( "#".repeat(size), listOf("0".repeat(size).map{"$it".toInt()}))){
-        (lineAbove, rollsAbove),string ->
-        Pair(string, rollsAbove + listOf( string.rollsNorth(lineAbove, rollsAbove.last())))
-    }.second.drop(1)
-
 fun String.rollsWest() =
     foldIndexed(listOf<Int>()){i, result, c -> if (i == 0 || c == '#') result + listOf(0) else if (this[i -1] == '.' ) result + listOf(result.last() + 1) else result + listOf(result.last())}
 
 fun List<String>.rollsWest() = map(String::rollsWest)
-
-fun List<String>.mappedNorth():List<String> {
-    val rollsNorth = rollsNorth()
-    val map:MutableMap<Pair<Int,Int>, Char> = mutableMapOf()
-    forEachIndexed {row, string ->
-        string.forEachIndexed{col, c ->
-            if (c != '.') map[Pair(row - rollsNorth[row][col], col)] = c
-        }
-    }
-    return mapIndexed {row, string -> string.indices.map{col -> map[Pair(row, col)] ?: '.'   }.joinToString("")}
-}
 
 fun List<String>.mappedWest():List<String> {
     val rollsWest = rollsWest()
@@ -53,16 +50,19 @@ fun List<String>.mappedWest():List<String> {
     return mapIndexed {row, string -> string.indices.map{col -> map[Pair(row, col)] ?: '.'   }.joinToString("")}
 }
 
+fun List<String>.mappedNorth():List<String> {
+    val rollsNorth = rollsNorth()
+    val map:MutableMap<Pair<Int,Int>, Char> = mutableMapOf()
+    forEachIndexed {row, string ->
+        string.forEachIndexed{col, c ->
+            if (c != '.') map[Pair(row - rollsNorth[row][col], col)] = c
+        }
+    }
+    return mapIndexed {row, string -> string.indices.map{col -> map[Pair(row, col)] ?: '.'   }.joinToString("")}
+}
+
 fun List<String>.cycled():List<String> =
     mappedNorth().mappedWest().reversed().mappedNorth().reversed().map{it.reversed()}.mappedWest().map { it.reversed() }
-
-
-fun List<String>.weightOnEachRow(rollsAbove:List<List<Int>>) =
-    mapIndexed{ row, string ->
-        string.mapIndexed{col, c ->
-            if (c == 'O') size - (row -  rollsAbove[row][col]) else 0
-        }.sum()
-    }
 
 fun List<String>.weight() = mapIndexed {i, s -> (size - i) * s.count { it == 'O' } }.sum()
 
