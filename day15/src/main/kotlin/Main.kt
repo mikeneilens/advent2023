dfa2dd30caad11362204619da50f172a105ec46d
Mike Neilens <mike.neilens@gmail.com>
2023-12-15 17:09:11 +0000

    day 15

--- day15/src/main/kotlin/Main.kt
+++ day15/src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
-}
-
 fun partOne(sampleData:String) :Int {
     return sampleData.split(",").sumOf(String::hashAlgorithm)
 }
@@ -10,5 +5,85 @@ fun partOne(sampleData:String) :Int {
 fun String.hashAlgorithm() = fold(0){current, char -> ((current + char.code) * 17).rem(256) }
 
 fun partTwo(sampleData:String):Int {
-    return 0
+    val map:MutableMap<Int, Lens> = mutableMapOf()
+    sampleData.parse().forEach { it.execute(map) }
+    return map.toList().sumOf {(key, value) -> (key + 1) * value.power() }
+}
+
+fun String.parse() =
+    split(",").map{string ->
+        if (string.last() == '-') RemoveLens(string.dropLast(1))
+        else UpsertLens(string.split("=").first(), string.split("=").last().toInt() )
+    }
+
+
+class Lens(val name:String, val focalLength:Int, var prev:Lens? = null, var next:Lens? = null) {
+
+    fun findLensOrNull(name:String):Lens? = if (name == this.name) this else if (next == null) null else next?.findLensOrNull(name)
+
+    fun lastLens():Lens? = if (next == null) this else next?.lastLens()
+
+    fun removeLens(name:String) {
+        findLensOrNull(name)?.let { lens ->
+            lens.prev?.next = lens.next
+            lens.next?.prev = lens.prev
+            lens.prev = null
+            lens.next = null
+        }
+    }
+    fun upsertLens(newLens:Lens) {
+        findLensOrNull(newLens.name)?.replaceLensWith(newLens) ?: lastLens()?.insertLens(newLens)
+    }
+
+    fun replaceLensWith(newLens:Lens) {
+        newLens.prev = prev
+        newLens.next = next
+        prev?.next = newLens
+        next?.prev = newLens
+        prev = null
+        next = null
+    }
+    fun insertLens(newLens:Lens) {
+        next = newLens
+        newLens.prev = this
+    }
+
+    fun power(num:Int = 1):Int = num * focalLength + (next?.power(num + 1) ?: 0)
+
+    override fun toString() = "Lens(name=\"$name\", focalLength=$focalLength, next=$next)"
+}
+
+sealed class LensInstruction(val name:String) {
+    val box = name.hashAlgorithm()
+    abstract fun execute(map: MutableMap<Int, Lens>)
+}
+
+class UpsertLens(name:String, val focalLength:Int):LensInstruction(name){
+    override fun execute(map: MutableMap<Int, Lens>) {
+        if (map.contains(box)) {
+            val newLens = Lens(name, focalLength)
+            map.getValue(box).upsertLens(newLens)
+            if (map[box]?.name == name) map[box] = newLens
+        } else {
+            map[box] = Lens(name, focalLength)
+        }
+    }
+
+    override fun toString() = "UpsertLens(name=\"$name\", focalLength=$focalLength)"
+    override fun equals(other: Any?) = other is UpsertLens && name == other.name
+}
+
+class RemoveLens(name:String):LensInstruction(name) {
+    override fun execute(map: MutableMap<Int, Lens>) {
+        if (map.contains(box)) {
+            if (map[box]?.name == name) {
+                map[box]?.next?.let{ map[box] = it} ?: map.remove(box)
+            } else {
+                map[box]?.removeLens(name)
+            }
+        }
+    }
+
+    override fun toString() = "RemoveLens(name=\"$name\")"
+    override fun equals(other: Any?) = other is RemoveLens && name == other.name
 }
--- day15/src/test/kotlin/MainTest.kt
+++ day15/src/test/kotlin/MainTest.kt
@@ -15,8 +15,73 @@ class MainTest:WordSpec({
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        "add lens to list of lenses" {
+            val lens = Lens("l1",1)
+            val newLens = Lens("l2",2)
+            lens.upsertLens(newLens)
+            lens.next shouldBe newLens
+            newLens.prev shouldBe lens
+            newLens.next shouldBe null
+        }
+        "update lens in a list of lenses" {
+            val lens1 = Lens("l1",1)
+            val lens2 = Lens("l2",2, prev = lens1); lens1.next = lens2
+            val lens3 = Lens("l3",3, prev = lens2); lens2.next = lens3
+            val newLens2 = Lens("l2",4)
+            lens1.upsertLens(newLens2)
+            lens1.next shouldBe newLens2
+            lens3.prev shouldBe newLens2
+            lens2.prev shouldBe null
+            lens2.next shouldBe null
+            newLens2.prev shouldBe lens1
+            newLens2.next shouldBe lens3
+        }
+        "update first lens in a list of lenses" {
+            val lens1 = Lens("l1",1)
+            val lens2 = Lens("l2",2, prev = lens1); lens1.next = lens2
+            val lens3 = Lens("l3",3, prev = lens2); lens2.next = lens3
+            val newLens1 = Lens("l1",4)
+            lens1.upsertLens(newLens1)
+            lens2.prev shouldBe newLens1
+            newLens1.prev shouldBe null
+            newLens1.next shouldBe lens2
+        }
+
+        "remove lens in a list of lenses" {
+            val lens1 = Lens("l1",1)
+            val lens2 = Lens("l2",2, prev = lens1); lens1.next = lens2
+            val lens3 = Lens("l3",3, prev = lens2); lens2.next = lens3
+            lens1.removeLens("l2")
+            lens1.next shouldBe lens3
+            lens3.prev shouldBe lens1
+            lens2.prev shouldBe null
+            lens2.next shouldBe null
+        }
+        "parse test data" {
+            testData.parse() shouldBe listOf(
+                UpsertLens(name="rn", focalLength=1),
+                RemoveLens(name="cm"),
+                UpsertLens(name="qp", focalLength=3),
+                UpsertLens(name="cm", focalLength=2),
+                RemoveLens(name="qp"),
+                UpsertLens(name="pc", focalLength=4),
+                UpsertLens(name="ot", focalLength=9),
+                UpsertLens(name="ab", focalLength=5),
+                RemoveLens(name="pc"),
+                UpsertLens(name="pc", focalLength=6),
+                UpsertLens(name="ot", focalLength=7)
+            )
+        }
+        "power of two lenses" {
+            val lens1 = Lens("l1",1)
+            val lens2 = Lens("l2",2, prev = lens1); lens1.next = lens2
+            lens1.power() shouldBe 5
+        }
+        "part two with test data should be 145" {
+            partTwo(testData) shouldBe 145
+        }
+        "part two with sample data should be 230197" {
+            partTwo(sampleData) shouldBe 230197
         }
     })
 })
