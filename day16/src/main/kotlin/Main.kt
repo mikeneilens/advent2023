2e9e97a74f8b4cf85f217883faecb4bf19695186
Mike Neilens <mike.neilens@gmail.com>
2023-12-16 13:33:20 +0000

    day16 part two

--- day16/src/main/kotlin/Main.kt
+++ day16/src/main/kotlin/Main.kt
@@ -1,25 +1,11 @@
 fun partOne(sampleData:List<String>, beam:Beam = Beam(Position(0,-1),Direction.Right)) :Int {
     val grid = sampleData.toGrid()
-    val beams = grid.fireBeam(beam)
+    val beams = beam.move(grid)
     return grid.visitedBy(beams).size
 }
 
 fun List<String>.toGrid() =
     flatMapIndexed{ row, s -> s.mapIndexed{ col, c -> Pair(Position(row, col),c)} }.toMap()
-
-fun Map<Position, Char>.fireBeam(beam:Beam):Set<Beam> {
-    val beams = mutableSetOf<Beam>()
-    beam.move(this, beams)
-    return beams
-}
-
-fun Beam.move(grid:Map<Position, Char>, beams:MutableSet<Beam> = mutableSetOf()) {
-    if (this in beams) return else beams.add(this)
-    val newPosition = position + direction.offset
-    if (newPosition !in grid.keys) return
-    val collision = Collision(grid.getValue(newPosition), direction)
-    resultsOfCollision[collision]?.forEach {newDiretion -> Beam(newPosition, newDiretion).move(grid,beams) }
-}
 
 fun Map<Position, Char>.visitedBy(beams:Set<Beam>):List<Position> {
     val positionOfBeams = beams.map{it.position}.distinct()
@@ -37,7 +23,23 @@ enum class Direction(val offset:Position) {
     Down(Position(row = +1, col = 0)),
 }
 
-data class Beam(val position:Position, val direction:Direction)
+data class Beam(val position:Position, val direction:Direction) {
+
+    fun move(grid:Map<Position, Char>):Set<Beam> {
+        val beams = mutableSetOf<Beam>()
+        move(grid, beams)
+        return beams
+    }
+
+    fun move(grid:Map<Position, Char>, beams:MutableSet<Beam> = mutableSetOf()) {
+        if (this in beams) return else beams.add(this)
+        val newPosition = position + direction.offset
+        if (newPosition !in grid.keys) return
+        val collision = Collision(grid.getValue(newPosition), direction)
+        resultsOfCollision[collision]?.forEach {newDiretion -> Beam(newPosition, newDiretion).move(grid,beams) }
+    }
+
+}
 
 data class Collision(val mirror:Char, val direction:Direction)
 
@@ -69,7 +71,7 @@ fun partTwo(sampleData:List<String>):Int {
     val maxRow = sampleData.lastIndex
     val maxCol = sampleData.first().lastIndex
     val beams = grid.beamFromLeft() + grid.beamFromTop() + grid.beamFromRight(maxCol) + grid.beamFromBottom(maxRow)
-    return beams.map { beam -> grid.visitedBy( grid.fireBeam(beam)).size }.max()
+    return beams.map { beam -> grid.visitedBy( beam.move(grid)).size }.max()
 }
 
 fun Map<Position, Char>.beamFromLeft() =
