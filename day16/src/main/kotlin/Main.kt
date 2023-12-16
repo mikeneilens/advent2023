22bea7b8fdb0be2febdcc64bfd3794399f98e82c
Mike Neilens <mike.neilens@gmail.com>
2023-12-16 11:36:34 +0000

    day16 part one

--- day16/src/main/kotlin/Main.kt
+++ day16/src/main/kotlin/Main.kt
@@ -1,11 +1,64 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+fun partOne(sampleData:List<String>) :Int {
+    val beams = mutableSetOf<Beam>()
+    val grid = sampleData.toGrid()
+    Beam(Position(0,-1),Direction.Right).move(grid, beams)
+    return grid.visitedBy(beams).size
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun List<String>.toGrid() =
+    flatMapIndexed{ row, s -> s.mapIndexed{ col, c -> Pair(Position(row, col),c)} }.toMap()
+
+fun Beam.move(grid:Map<Position, Char>, beams:MutableSet<Beam> = mutableSetOf()) {
+    if (this in beams) return else beams.add(this)
+    val beam = copy(position = position + direction.offset )
+    if (beam.position !in grid.keys) return
+    val collision = Collision(grid.getValue(beam.position),beam.direction)
+    resultsOfCollision[collision]?.forEach { beam.copy(direction = it).move(grid,beams) }
 }
-fun partTwo(sampleData:String):Int {
+
+fun Map<Position, Char>.visitedBy(beams:Set<Beam>):List<Position> {
+    val positionOfBeams = beams.map{it.position}.distinct()
+    return keys.filter{position -> position in positionOfBeams}
+}
+
+data class Position(val row:Int, val col:Int) {
+    operator fun plus(other:Position) = Position(row + other.row, col + other.col)
+}
+
+enum class Direction(val offset:Position) {
+    Right(Position(row = 0, col = +1)),
+    Left(Position(row = 0, col = -1)),
+    Up(Position(row = -1, col = 0)),
+    Down(Position(row = +1, col = 0)),
+}
+
+data class Beam(val position:Position, val direction:Direction)
+
+data class Collision(val mirror:Char, val direction:Direction)
+
+val resultsOfCollision = mapOf(
+    Collision('-', Direction.Right) to listOf(Direction.Right),
+    Collision('-', Direction.Left) to listOf(Direction.Left),
+    Collision('-', Direction.Up) to listOf(Direction.Left, Direction.Right),
+    Collision('-', Direction.Down) to listOf(Direction.Left, Direction.Right),
+    Collision('|', Direction.Right) to listOf(Direction.Up, Direction.Down),
+    Collision('|', Direction.Left) to listOf(Direction.Up, Direction.Down),
+    Collision('|', Direction.Up) to listOf(Direction.Up),
+    Collision('|', Direction.Down) to listOf(Direction.Down),
+    Collision('\\', Direction.Right) to listOf(Direction.Down),
+    Collision('\\', Direction.Left) to listOf(Direction.Up),
+    Collision('\\', Direction.Up) to listOf(Direction.Left),
+    Collision('\\', Direction.Down) to listOf(Direction.Right),
+    Collision('/', Direction.Right) to listOf(Direction.Up),
+    Collision('/', Direction.Left) to listOf(Direction.Down),
+    Collision('/', Direction.Up) to listOf(Direction.Right),
+    Collision('/', Direction.Down) to listOf(Direction.Left),
+    Collision('.', Direction.Right) to listOf(Direction.Right),
+    Collision('.', Direction.Left) to listOf(Direction.Left),
+    Collision('.', Direction.Up) to listOf(Direction.Up),
+    Collision('.', Direction.Down) to listOf(Direction.Down)
+)
+
+fun partTwo(sampleData:List<String>):Int {
     return 0
 }
--- day16/src/test/kotlin/MainTest.kt
+++ day16/src/test/kotlin/MainTest.kt
@@ -2,9 +2,31 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testData = """
+        .|...\....
+        |.-.\.....
+        .....|-...
+        ........|.
+        ..........
+        .........\
+        ..../.\\..
+        .-.-/..|..
+        .|....-|.\
+        ..//.|....
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "move beam starting at top left of grid" {
+            val beams = mutableSetOf<Beam>()
+            val grid = testData.toGrid()
+            Beam(Position(0,0),Direction.Right).move(grid, beams)
+            grid.visitedBy(beams).size shouldBe 46
+        }
+        "part one with test data should be 0" {
+             partOne(testData) shouldBe 46
+        }
+        "part one with sample data should be 6740" {
+            partOne(sampleData) shouldBe 6740
         }
     })
     "part two" should ({
@@ -13,3 +35,12 @@ class MainTest:WordSpec({
         }
     })
 })
+
+fun printData(grid:Map<Position, Char>, beams:Set<Position>) {
+    (0..49).forEach { row ->
+        (0..49).forEach { col ->
+            if (grid[Position(row, col)] == '.' && Position(row, col) in beams) print('B') else print(grid[Position(row, col)])
+        }
+        println()
+    }
+}
