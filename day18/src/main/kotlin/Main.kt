c00314925c98d329c3d6ce33965c8e32a78d4a57
Mike Neilens <mike.neilens@gmail.com>
2023-12-18 19:00:40 +0000

    day18 part two

--- day18/src/main/kotlin/Main.kt
+++ day18/src/main/kotlin/Main.kt
@@ -1,11 +1,102 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+import kotlin.math.absoluteValue
+
+fun partOne(sampleData:List<String>) :Long {
+    val map = mutableMapOf<Position, String>()
+    sampleData.toInstructions().fold(Position(0,0)){ position, instruction ->
+        instruction.process(position, map)
+    }
+    map.addBoundary()
+    map.flood("2")
+    return map.count { it.value.startsWith("#") || it.value == "2" }.toLong()
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun List<String>.toInstructions() = map(String::toInstruction)
+
+fun String.toInstruction() = Instruction (
+    direction = charToDirection.getValue(split(" " )[0].first()),
+    steps = split(" ")[1].toLong(),
+    colour = split(" ")[2].removePrefix("(").removeSuffix(")"),
+)
+
+data class Instruction(val direction: Direction, val steps:Long, val colour:String) {
+    fun process(start:Position, map:MutableMap<Position, String>):Position {
+        (0 until steps).fold(start){ position, _ ->
+            map.addColour(position, direction, colour)
+            position + direction.offset
+        }
+        return start + direction.offset * steps
+    }
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+fun MutableMap<Position, String>.addBoundary(){
+    val rowStart = (keys.minOf { it.row } -1)
+    val rowEnd = (keys.maxOf { it.row } +1)
+    val colStart = (keys.minOf { it.col } -1)
+    val colEnd = (keys.maxOf { it.col } +1)
+
+    (rowStart..rowEnd).forEach { row -> this[Position(row, colStart)] = "X" }
+    (rowStart..rowEnd).forEach { row -> this[Position(row, colEnd)] = "X" }
+    (colStart..colEnd).forEach { col -> this[Position(rowStart, col)] = "X" }
+    (colStart..colEnd).forEach { col -> this[Position(rowEnd, col)] = "X" }
 }
+
+fun MutableMap<Position, String>.addColour(position: Position, direction: Direction, colour:String) {
+    this[position + direction.offset] = colour
+    if ((position + direction.side1) !in this) this[position + direction.side1] = "1"
+    if ((position + direction.side2) !in this) this[position + direction.side2] = "2"
+}
+
+fun MutableMap<Position, String>.flood(s:String) {
+    val startingSize = size
+    filter{it.value == s}.keys.forEach { position ->
+        listOf(Direction.Left, Direction.Right, Direction.Down, Direction.Up).forEach {
+                if ((position + it.offset) !in this) this[position + it.offset] = s
+        }
+    }
+    if (size > startingSize) flood(s)
+}
+
+
+data class Position(val row:Long, val col:Long) {
+    operator fun plus(other:Position) = Position(row + other.row, col + other.col)
+    operator fun times(t:Long) = Position(row * t, col * t)
+}
+
+enum class Direction(val offset:Position, val side1:Position, val side2:Position) {
+    Right(Position(row = 0, col = +1), Position(row = -1, col = +1), Position(row = +1, col = +1)),
+    Left(Position(row = 0, col = -1), Position(row = +1, col = -1), Position(row = -1, col = -1)),
+    Up(Position(row = -1, col = 0), Position(row = -1, col = -1), Position(row = -1, col = +1)),
+    Down(Position(row = +1, col = 0), Position(row = +1, col = +1), Position(row = +1, col = -1)),
+}
+
+val charToDirection = mapOf( 'R' to Direction.Right, 'L' to Direction.Left, 'U' to Direction.Up, 'D' to Direction.Down)
+
+fun partTwo(sampleData:List<String>):Long {
+    val instructions = sampleData.toInstruction2()
+    return areaUsingShoeLaceAndPicksTheorm(instructions.positions(), instructions.totalSteps())
+}
+
+fun List<String>.toInstruction2() = map(String::toInstruction2)
+
+fun String.toInstruction2() = Instruction(
+    charToDirection2.getValue(split(" ")[2].removeSuffix(")").last()),
+    split(" ")[2].removePrefix("(#").removeSuffix(")").dropLast(1).toLong(16),
+    ""
+)
+
+fun List<Instruction>.positions() = fold(listOf(Position(0,0))){positions, instruction ->
+    positions + listOf(positions.last() + (instruction.direction.offset * instruction.steps) )
+}
+fun List<Instruction>.totalSteps() = sumOf { it.steps }
+
+val charToDirection2 = mapOf( '0' to Direction.Right, '2' to Direction.Left, '3' to Direction.Up, '1' to Direction.Down)
+
+fun areaUsingShoeLaceAndPicksTheorm(positions:List<Position>, totalSteps: Long) = totalSteps + calcInternalAreaFromPicksTheorm(positions, totalSteps)
+
+fun calcAForShoeLaceTheorm(positions: List<Position>)
+    = (positions.mapIndexed {i, p -> p.row * ( positions[(i+1).specialMod(positions.size)].col - positions[(i-1).specialMod(positions.size)].col )}
+        .sum()/2 ).absoluteValue
+
+fun Int.specialMod(size:Int) = if (this < 0 ) size - 1 else this % size
+
+fun calcInternalAreaFromPicksTheorm(positions: List<Position>, totalSteps:Long) = calcAForShoeLaceTheorm(positions) - totalSteps/2 +1
--- day18/src/test/kotlin/MainTest.kt
+++ day18/src/test/kotlin/MainTest.kt
@@ -2,14 +2,73 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testData = """
+        R 6 (#70c710)
+        D 5 (#0dc571)
+        L 2 (#5713f0)
+        D 2 (#d2c081)
+        R 2 (#59c680)
+        D 2 (#411b91)
+        L 5 (#8ceee2)
+        U 2 (#caa173)
+        L 1 (#1b58a2)
+        U 2 (#caa171)
+        R 2 (#7807d2)
+        U 3 (#a77fa3)
+        L 2 (#015232)
+        U 2 (#7a21e3)
+    """.trimIndent().split("\n")
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "converting test data to instructions" {
+            val instructions = testData.toInstructions()
+            instructions.size shouldBe 14
+            instructions.first() shouldBe Instruction(Direction.Right, 6, "#70c710")
+            instructions.last() shouldBe Instruction(Direction.Up, 2, "#7a21e3")
+        }
+        "processing the first instruction adds 6 data to row 0 of the map" {
+            val map = mutableMapOf<Position, String>()
+            testData.first().toInstruction().process(Position(0,0),map) shouldBe Position(0,6)
+            map[Position(0,1)] shouldBe  "#70c710"
+            map[Position(0,2)] shouldBe  "#70c710"
+            map[Position(0,3)] shouldBe  "#70c710"
+            map[Position(0,4)] shouldBe  "#70c710"
+            map[Position(0,5)] shouldBe  "#70c710"
+            map[Position(0,6)] shouldBe  "#70c710"
+            map[Position(0,0)] shouldBe  null
+            map[Position(0,7)] shouldBe  null
+        }
+        "print testdata map after processing it" {
+            val map = mutableMapOf<Position, String>()
+            testData.toInstructions().fold(Position(0,0)){ position, instruction ->
+                instruction.process(position, map)
+            }
+            map.addBoundary()
+            map.flood("2")
+        }
+        "part one with test data should be 62" {
+            partOne(testData) shouldBe 62
+        }
+        "part one with sample data should be 46334" {
+            partOne(sampleData) shouldBe 46334
         }
     })
     "part two" should ({
-        "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+        "calcualte area for the test data using part one co-ordinates" {
+            val positions = testData.toInstructions().fold(listOf(Position(0,0))){positions, instruction ->
+                positions + listOf(positions.last() + (instruction.direction.offset * instruction.steps) )
+            }
+            val totalSteps = testData.toInstructions().sumOf { it.steps }
+            areaUsingShoeLaceAndPicksTheorm(positions, totalSteps) shouldBe 62
+        }
+        "convert string to instruction" {
+            "R 6 (#70c710)".toInstruction2() shouldBe Instruction(Direction.Right,461937,"")
+        }
+        "part two using test data should be 952408144115" {
+            partTwo(testData) shouldBe 952408144115L
+        }
+        "part two using sample data should be 102000662718092" {
+            partTwo(sampleData) shouldBe 102000662718092
         }
     })
+
 })
