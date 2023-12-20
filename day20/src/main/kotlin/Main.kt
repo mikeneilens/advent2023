7eb500627c0c47f95a853d72dd00df832191fc8e
Mike Neilens <mike.neilens@gmail.com>
2023-12-20 14:58:10 +0000

    day20 part two

--- day20/src/main/kotlin/Main.kt
+++ day20/src/main/kotlin/Main.kt
@@ -1,11 +1,113 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+
+fun partOne(sampleData:List<String>) :Long {
+    val modules = mutableMapOf<String, Module>()
+    sampleData.updateModules(modules)
+    repeat(1000){modules["button"]?.send(Pulse.Low)}
+    val pulsesSent = Pair(modules.values.sumOf { it.pulsesSent.filter { it == Pulse.Low }.size }.toLong(),
+                          modules.values.sumOf { it.pulsesSent.filter { it == Pulse.High }.size }.toLong())
+    return pulsesSent.first * pulsesSent.second
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun List<String>.updateModules(modules:MutableMap<String, Module>) {
+    filter(String::isBroadcaster).forEach { modules["broadcaster"] = it.toBroadcaster(modules) }
+    filter(String::isFlipFlop).forEach { modules[it.flipFlopName] = it.toFlipFlop(modules) }
+    filter(String::isConjunction).forEach { modules[it.conjunctionName] = it.toConjunction(modules) }
+    filter(String::isConjunction).forEach { modules[it.conjunctionName] = it.toConjunction(modules) }
+    modules["button"] = Button(modules, listOf("broadcaster"))
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+fun String.isBroadcaster() = startsWith("broad")
+fun String.isFlipFlop() = startsWith("%")
+fun String.isConjunction() = startsWith("&")
+
+fun String.toBroadcaster(modules:MutableMap<String, Module>) = Broadcaster(modules, destinations)
+
+fun String.toFlipFlop(modules:MutableMap<String, Module>) = FlipFlop(modules = modules, destinations =  destinations)
+
+fun String.toConjunction(modules:MutableMap<String, Module>) =
+    Conjunction(modules.withDestinationOf(conjunctionName), modules, destinations)
+
+val String.conjunctionName get() = drop(1).split(" -> ")[0]
+val String.flipFlopName get() = drop(1).split(" -> ")[0]
+val String.destinations get() = split(" -> ")[1].split(", ")
+
+fun MutableMap<String, Module>.withDestinationOf(s:String) = filter { (_, value) -> value.destinations.contains(s) }.keys.toList()
+
+enum class Pulse{High, Low}
+enum class Status{On, Off}
+
+interface Module {
+    val modules:Map<String, Module>
+    val destinations:List<String>
+    val pulsesSent:MutableList<Pulse>
+
+    fun receive(pulse:Pulse)
+    fun send(pulse:Pulse) {
+        destinations.forEach {pulsesSent.add(pulse); modules[it]?.receive(pulse) }
+    }
 }
+
+data class Button(override val modules:Map<String, Module>, override val destinations:List<String>, override val pulsesSent:MutableList<Pulse> = mutableListOf()):Module {
+    override fun receive(pulse: Pulse) {
+        send(pulse)
+    }
+    override fun toString() = "Button( destinations = $destinations pulsesSent = $pulsesSent)"
+}
+
+data class Broadcaster(override val modules:Map<String, Module>, override val destinations:List<String>, override val pulsesSent:MutableList<Pulse> = mutableListOf()):Module {
+    override fun receive(pulse: Pulse) {
+        send(pulse)
+    }
+    override fun toString() = "Broadcaster( destinations = $destinations pulsesSent = $pulsesSent)"
+}
+
+data class FlipFlop(var status:Status = Status.Off, override val modules:Map<String, Module>, override val destinations:List<String>, override val pulsesSent:MutableList<Pulse> = mutableListOf()):Module {
+    override fun receive(pulse: Pulse) {
+        if (pulse == Pulse.High) return
+        if (status == Status.Off) {
+            status = Status.On
+            send(Pulse.High)
+        } else {
+            status = Status.Off
+            send(Pulse.Low)
+        }
+    }
+    override fun toString() = "FlipFlop( status $status destinations = $destinations pulsesSent = $pulsesSent)"
+}
+
+data class Conjunction(val inputs:List<String>, override val modules:Map<String, Module>, override val destinations:List<String>, override val pulsesSent:MutableList<Pulse> = mutableListOf()):Module {
+    override fun receive(pulse: Pulse) {
+        if (inputs.isNotEmpty() && inputs.all{s -> if(modules[s]?.pulsesSent?.size == 0) false else modules[s]?.pulsesSent?.last() == Pulse.High})
+            send(Pulse.Low)
+        else
+            send(Pulse.High)
+    }
+    override fun toString() = "Conjunction( destinations = $destinations pulsesSent = $pulsesSent)"
+}
+
+fun partTwo(sampleData:List<String>):Long {
+    val modules = mutableMapOf<String, Module>()
+    sampleData.updateModules(modules)
+    val pressesForFT = buttonPressesToPulseHigh(modules, "ft")
+    val pressesForJZ = buttonPressesToPulseHigh(modules, "jz")
+    val pressesForSV = buttonPressesToPulseHigh(modules, "sv")
+    val pressesForNG = buttonPressesToPulseHigh(modules, "ng")
+
+    return lowestCommonMultiple(listOf( pressesForFT, pressesForJZ, pressesForNG, pressesForSV))
+}
+
+fun buttonPressesToPulseHigh(modules:MutableMap<String, Module>,module:String):Long {
+    sampleData.updateModules(modules)
+    var count = 0
+    while ( Pulse.High !in  (modules[module]?.pulsesSent ?: listOf())) {
+        modules["button"]?.send(Pulse.Low)
+        count++
+    }
+    return count.toLong()
+}
+
+fun lowestCommonMultiple(numbers: List<Long>): Long =
+    numbers.fold(1L) { x, y -> x * (y / greatestCommonDenominator(x, y)) }
+
+fun greatestCommonDenominator(x: Long, y: Long):Long =
+    if (y == 0L) x else greatestCommonDenominator(y, x % y)
--- day20/src/test/kotlin/MainTest.kt
+++ day20/src/test/kotlin/MainTest.kt
@@ -2,9 +2,82 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+
+    val testdata1 = """
+        broadcaster -> a, b, c
+        %a -> b
+        %b -> c
+        %c -> inv
+        &inv -> a
+    """.trimIndent().split("\n")
+
+    val testdata2 = """
+        broadcaster -> a
+        %a -> inv, con
+        &inv -> b
+        %b -> con
+        &con -> output
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "parse broadcaster -> a, b, c into a broadcast module" {
+            val modules = mutableMapOf<String, Module>()
+            "broadcaster -> a, b, c".toBroadcaster(modules) shouldBe Broadcaster(modules, listOf("a","b","c"))
+        }
+        "parse %b -> c into a flip-flop module "  {
+            val modules = mutableMapOf<String, Module>()
+            "%b -> c".toFlipFlop(modules) shouldBe FlipFlop(Status.Off, modules, listOf("c"))
+        }
+        "parse &inv -> a into a conjunction module "  {
+            val modules = mutableMapOf<String, Module>()
+            modules["c"] = Broadcaster(modules, listOf("a","inv","c"))
+            modules["d"] = Broadcaster(modules, listOf("a","c"))
+            modules.withDestinationOf("inv") shouldBe listOf("c")
+            val module = "&inv -> a".toConjunction(modules)
+            module.inputs shouldBe listOf("c")
+        }
+        "update module map with test data" {
+            val modules = mutableMapOf<String, Module>()
+            testdata1.updateModules(modules)
+            val broadcaster =  modules["broadcaster"]
+            val a = modules["a"]
+            val b = modules["b"]
+            val c = modules["c"]
+            val inv = modules["inv"]
+            broadcaster?.destinations shouldBe listOf("a","b","c")
+            a?.destinations shouldBe listOf("b")
+            b?.destinations shouldBe listOf("c")
+            c?.destinations shouldBe listOf("inv")
+            inv?.destinations shouldBe listOf("a")
+        }
+        "sending a low pulse to broadcast in testdata1" {
+            val modules = mutableMapOf<String,Module>()
+            testdata1.updateModules(modules)
+            modules["button"]?.send(Pulse.Low)
+            val lowPulseSent =  modules.values.sumOf { it.pulsesSent.filter { it == Pulse.Low }.size }
+            val highPulseSent =  modules.values.sumOf { it.pulsesSent.filter { it == Pulse.High }.size }
+            lowPulseSent shouldBe 8
+            highPulseSent shouldBe 4
+            modules.forEach { println("${it.key} ${it.value.pulsesSent}") }
+        }
+        "sending a low pulse to broadcast in testdata2" {
+            val modules = mutableMapOf<String,Module>()
+            testdata2.updateModules(modules)
+            modules["button"]?.send(Pulse.Low)
+//            val lowPulseSent =  modules.values.sumOf { it.pulsesSent.filter { it == Pulse.Low }.size }
+//            val highPulseSent =  modules.values.sumOf { it.pulsesSent.filter { it == Pulse.High }.size }
+//            lowPulseSent shouldBe 8
+//            highPulseSent shouldBe 4
+//            modules.forEach { println("${it.key} ${it.value.pulsesSent}") }
+        }
+        "part one with testdata1 should be 32000000L" {
+            partOne(testdata1) shouldBe 32000000L
+        }
+        "part one with testdata2 should be 11687500L" {
+            partOne(testdata2) shouldBe 11687500L
+        }
+        "part one with sample data should be 11687500L" {
+            partOne(sampleData) shouldBe 11687500L
         }
     })
     "part two" should ({
