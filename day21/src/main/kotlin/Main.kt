c199ae298e84c48ad79b4ee3ec5dec3755b7a010
Mike Neilens <mike.neilens@gmail.com>
2023-12-21 10:47:30 +0000

    day21 part one

--- day21/src/main/kotlin/Main.kt
+++ day21/src/main/kotlin/Main.kt
@@ -1,11 +1,39 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+
+data class Position(val row:Int, val col:Int) {
+    operator fun plus(other:Position) = Position(row + other.row, col + other.col)
+    operator fun times(t:Int) = Position(row * t, col * t)
+
+    fun surroundingPositions(map:Set<Position>) = Direction.values().map{this + it.offset}.filter { it.isOn(map) }
+
+    fun isOn(map:Set<Position>) = this in map
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+enum class Direction(val offset:Position) {
+    East(Position(row = 0, col = +1)),
+    West(Position(row = 0, col = -1)),
+    North(Position(row = -1, col = 0),),
+    South(Position(row = +1, col = 0),),
 }
-fun partTwo(sampleData:String):Int {
+
+fun partOne(sampleData:List<String>) :Int {
+    val s = sampleData.positionOfS()
+    val map = sampleData.createMap()
+    return walk(listOf(s),0, 64, map).size
+}
+
+fun List<String>.positionOfS() = Position(indexOfFirst{it.contains('S')},  this[indexOfFirst{it.contains('S')}].indexOfFirst { it == 'S' })
+
+fun List<String>.createMap():Set<Position> =
+    flatMapIndexed { row, string -> string.mapIndexed {col, char ->
+            if (char in listOf('.','S')) Position(row, col) else null
+        } }.filterNotNull().toSet()
+
+tailrec fun walk(positions:List<Position>, steps:Int, maxSteps:Int, map:Set<Position>):List<Position> {
+    if (steps == maxSteps) return positions
+    val newPositions = positions.flatMap { position -> position.surroundingPositions(map) }.distinct()
+    return walk(newPositions, steps + 1, maxSteps, map)
+}
+
+fun partTwo(sampleData:List<String>):Int {
     return 0
 }
--- day21/src/test/kotlin/MainTest.kt
+++ day21/src/test/kotlin/MainTest.kt
@@ -2,9 +2,44 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testdata1 = """
+        ...........
+        .....###.#.
+        .###.##..#.
+        ..#.#...#..
+        ....#.#....
+        .##..S####.
+        .##..#...#.
+        .......##..
+        .##.#.####.
+        .##..##.##.
+        ...........
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+
+        "surrounding postiions in testdata1 of S are "{
+            val map = testdata1.createMap()
+            val positionOfS = Position(5,5)
+            positionOfS.surroundingPositions(map) shouldBe listOf(Position(row=5, col=4), Position(row=4, col=5))
+        }
+        "walking 1 steps with test data 1 gives 2 locations" {
+            val map = testdata1.createMap()
+            val positionOfS = Position(5,5)
+            walk(listOf(positionOfS),0,1,map) shouldBe listOf(Position(row=5, col=4), Position(row=4, col=5))
+        }
+        "walking 2 steps with test data 1 gives 4 locations" {
+            val map = testdata1.createMap()
+            val positionOfS = Position(5,5)
+            walk(listOf(positionOfS),0,2,map) shouldBe listOf(Position(row=5, col=5), Position(row=5, col=3), Position(row=6, col=4), Position(row=3, col=5))
+        }
+        "walking 6 steps with test data 1 gives 16 locations" {
+            val map = testdata1.createMap()
+            val positionOfS = Position(5,5)
+            walk(listOf(positionOfS),0,6,map).size shouldBe 16
+        }
+        "part one should be 3642" {
+            partOne(sampleData) shouldBe 3642
         }
     })
     "part two" should ({
