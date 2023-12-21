6c4309be6e58c5f5ec80fade2617ab22580440a6
Mike Neilens <mike.neilens@gmail.com>
2023-12-21 15:56:00 +0000

    day21 part two

--- day21/src/main/kotlin/Main.kt
+++ day21/src/main/kotlin/Main.kt
@@ -1,41 +1,64 @@
+import kotlin.math.pow
 
-data class Position(val row:Int, val col:Int) {
+data class Position(val row:Long, val col:Long) {
     operator fun plus(other:Position) = Position(row + other.row, col + other.col)
-    operator fun times(t:Int) = Position(row * t, col * t)
 
-    fun surroundingPositions(map:Set<Position>, size:Int) = Direction.values().map{this + it.offset}.filter { it.isOn(map, size) }
+    fun surroundingPositions(map:Set<Position>) = offsets.map{offset -> this + offset}.filter { it.isOn(map) }
 
-    fun isOn(map:Set<Position>, size:Int) = Position( row % size, col % size ) in map
+    fun isOn(map:Set<Position>) = Position( row , col  ) in map
+
+    companion object {
+        val offsets = listOf(Position(row = 0, col = +1), Position(row = 0, col = -1), Position(row = -1, col = 0), Position(row = +1, col = 0))
+    }
 }
 
-fun Int.remainder(other:Int) = this % other
-
-enum class Direction(val offset:Position) {
-    East(Position(row = 0, col = +1)),
-    West(Position(row = 0, col = -1)),
-    North(Position(row = -1, col = 0),),
-    South(Position(row = +1, col = 0),),
-}
-
-fun partOne(sampleData:List<String>) :Int {
-    val s = sampleData.positionOfS()
+fun partOne(sampleData:List<String>) :Long {
+    val positionOfS = sampleData.positionOfS()
     val map = sampleData.createMap()
-    return walk(listOf(s),0, 64, map, sampleData.size).size
+    return map.walk(positionOfS, 64).toLong()
 }
 
-fun List<String>.positionOfS(n:Int = 1) = Position( n * size + size/2, n * size + size/2)
+fun List<String>.positionOfS(n:Long = 0) = Position( n * size + size/2, n * size + size/2)
 
 fun List<String>.createMap():Set<Position> =
     flatMapIndexed { row, string -> string.mapIndexed {col, char ->
-            if (char in listOf('.','S')) Position(row, col) else null
+            if (char in listOf('.','S')) Position(row.toLong(), col.toLong()) else null
         } }.filterNotNull().toSet()
 
-tailrec fun walk(positions:List<Position>, steps:Int, maxSteps:Int, map:Set<Position>, size:Int):List<Position> {
+fun  Set<Position>.walk(position:Position, maxSteps:Long) = walk(listOf(position),0, maxSteps, this).size
+
+tailrec fun walk(positions:List<Position>, steps:Long, maxSteps:Long, map:Set<Position>):List<Position> {
     if (steps == maxSteps) return positions
-    val newPositions = positions.flatMap { position -> position.surroundingPositions(map, size) }.distinct()
-    return walk(newPositions, steps + 1, maxSteps, map, size)
+    val newPositions = positions.flatMap { position -> position.surroundingPositions(map) }.distinct()
+    return walk(newPositions, steps + 1, maxSteps, map)
 }
 
-fun partTwo(sampleData:List<String>):Int {
-    return 0
+fun partTwo(sampleData:List<String>):Long {
+    val map = sampleData.createMap()
+    val steps = 26501365L
+    val size = sampleData.size.toLong()
+    val gridWidth = steps / size - 1
+    val positionOfS = sampleData.positionOfS()
+
+    val odd = ((gridWidth / 2) * 2 + 1).toDouble().pow(2).toLong()
+    val even = (((gridWidth + 1) / 2) * 2).toDouble().pow(2).toLong()
+
+    return odd * map.oddSteps(size, positionOfS) +
+            even * map.evenSteps(size, positionOfS) +
+            map.stepsInCorners(size, positionOfS) +
+            (gridWidth + 1) * map.stepsInSmallEdges(size) +
+            gridWidth * map.stepsInLargeEdges(size)
 }
+
+fun Set<Position>.oddSteps(size: Long, s: Position) = walk(s, size * 2 + 1 )
+
+fun Set<Position>.evenSteps(size: Long, s: Position) = walk(s, size * 2  )
+
+fun  Set<Position>.stepsInCorners(size: Long, s: Position) =
+        listOf(Position(size - 1, s.col), Position(s.row, 0), Position(0, s.col), Position(s.row, size - 1)).map { walk(it, size - 1) }.sum()
+
+fun  Set<Position>.stepsInSmallEdges(size: Long) =
+    listOf(Position(size - 1 , 0),Position(size - 1 , size - 1),Position(0 , 0),Position(0 , size -1 )).map{walk(it,size/2 - 1)}.sum()
+
+fun  Set<Position>.stepsInLargeEdges(size: Long) =
+    listOf(Position(size -1  , 0 ),Position(size -1  , size -1 ),Position(0  , 0 ),Position(0  , size -1  )).map{walk(it,size * 3 /2 - 1)}.sum()
--- day21/src/test/kotlin/MainTest.kt
+++ day21/src/test/kotlin/MainTest.kt
@@ -21,62 +21,30 @@ class MainTest:WordSpec({
         "surrounding postiions in testdata1 of S are "{
             val map = testdata1.createMap()
             val positionOfS = Position(5,5)
-            positionOfS.surroundingPositions(map,11) shouldBe listOf(Position(row=5, col=4), Position(row=4, col=5))
+            positionOfS.surroundingPositions(map) shouldBe listOf(Position(row=5, col=4), Position(row=4, col=5))
         }
         "walking 1 steps with test data 1 gives 2 locations" {
             val map = testdata1.createMap()
             val positionOfS = Position(5,5)
-            walk(listOf(positionOfS),0,1,map,11) shouldBe listOf(Position(row=5, col=4), Position(row=4, col=5))
+            walk(listOf(positionOfS),0,1,map) shouldBe listOf(Position(row=5, col=4), Position(row=4, col=5))
         }
         "walking 2 steps with test data 1 gives 4 locations" {
             val map = testdata1.createMap()
             val positionOfS = Position(5,5)
-            walk(listOf(positionOfS),0,2,map,11) shouldBe listOf(Position(row=5, col=5), Position(row=5, col=3), Position(row=6, col=4), Position(row=3, col=5))
+            walk(listOf(positionOfS),0,2,map) shouldBe listOf(Position(row=5, col=5), Position(row=5, col=3), Position(row=6, col=4), Position(row=3, col=5))
         }
         "walking 6 steps with test data 1 gives 16 locations" {
             val map = testdata1.createMap()
             val positionOfS = Position(5,5)
-            walk(listOf(positionOfS),0,6,map, 11).size shouldBe 16
+            walk(listOf(positionOfS),0,6,map).size shouldBe 16
         }
         "part one should be 3642" {
             partOne(sampleData) shouldBe 3642
         }
     })
     "part two" should ({
-
-        "column is on map" {
-            val map = testdata1.createMap()
-            Position(5,10).isOn(map, 11) shouldBe true
-            Position(5,11).isOn(map, 11) shouldBe true
-            Position(5,12).isOn(map, 11) shouldBe false
-            Position(5,13).isOn(map, 11) shouldBe false
-            Position(5,14).isOn(map, 11) shouldBe true
-            Position(5,15).isOn(map, 11) shouldBe true
-            Position(5,16).isOn(map, 11) shouldBe true
-            Position(5,17).isOn(map, 11) shouldBe false
-        }
-        "row is on map" {
-            val map = testdata1.createMap()
-            Position(10,5).isOn(map, 11) shouldBe true
-            Position(11,5).isOn(map, 11) shouldBe true
-            Position(12,5).isOn(map, 11) shouldBe false
-            Position(13,5).isOn(map, 11) shouldBe false
-            Position(0,5).isOn(map, 11) shouldBe true
-        }
-        "walking 10 steps with test data 1 gives 50 locations" {
-            val map = testdata1.createMap()
-            val positionOfS = testdata1.positionOfS(2)
-            val x = walk(listOf(positionOfS),0,10,map, 11)
-            x.size shouldBe 50
-        }
-        "walking 50 steps with test data 1 gives 1594 locations" {
-            val map = testdata1.createMap()
-            val positionOfS = testdata1.positionOfS(4)
-            val x = walk(listOf(positionOfS),0,50,map, 11)
-            x.size shouldBe 1594
-        }
         "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+            partTwo(sampleData) shouldBe 608603023105276L
         }
     })
 })
