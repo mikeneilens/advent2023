c7671ba2e833335f78241c0dbac3034a7ea6df66
Mike Neilens <mike.neilens@gmail.com>
2023-12-23 21:25:10 +0000

    day23

--- day23/src/main/kotlin/Main.kt
+++ day23/src/main/kotlin/Main.kt
@@ -1,11 +1,75 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+
+fun partOne(sampleData:List<String>) :Int {
+    val grid = sampleData.Grid()
+    val paths = grid.startPostition().findPath(grid, grid.endPostition(), setOf(grid.startPostition()) )
+    return paths.maxOf { it.size } - 1
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun Position.findPath(grid:Grid, endPosition:Position, visited:Set<Position> ):List<Set<Position>> {
+    if (this == endPosition) return listOf(visited)
+    return surroundingPositions(grid).filter { it !in visited  }.flatMap{position ->
+        position.findPath(grid,endPosition, visited + position )
+    }
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+typealias Grid = Map<Position,Char>
+
+fun List<String>.Grid() = flatMapIndexed{ row, s -> s.mapIndexed { col, c -> Pair(Position(row, col),c)}}.toMap()
+fun Grid.startPostition() = filter{it.key.row == 0 && it.value == '.' }.keys.first()
+fun Grid.endPostition() = filter{it.key.row == keys.maxOf { it.row } && it.value == '.' }.keys.first()
+
+data class Position(val row:Int, val col:Int) {
+    operator fun plus(other:Position) = Position(row + other.row, col + other.col)
+
+    fun surroundingPositions(grid:Grid) = offsets.getValue(grid.getValue(this)).map{ offset -> this + offset}.filter { grid[it] != '#' && it.isOn(grid)}
+
+    fun isOn(grid:Grid) = Position( row , col  ) in grid
+
+    companion object {
+        val offsets = mapOf(
+            '.' to listOf(Position(row = 0, col = +1), Position(row = 0, col = -1), Position(row = -1, col = 0), Position(row = +1, col = 0)),
+            '>' to listOf(Position(row =  0, col = +1)),
+            '^' to listOf(Position(row = -1, col =  0)),
+            'v' to listOf(Position(row = +1, col =  0)),
+            '<' to listOf(Position(row =  0, col = -1)),
+        )
+    }
 }
+
+fun partTwo(sampleData:List<String>):Int {
+    val grid = sampleData.Grid2()
+    val junctions = grid.findJunctions().toSet()
+    val connections:Map<Position,List<Pair<Position, Int>>> = junctions.findConnections(grid)
+    val paths = grid.startPostition().findPath2(connections, grid.endPostition(), setOf(), setOf(grid.startPostition()))
+    return paths.maxOf{ it.sumOf { it.second } }
+}
+
+var max = 0
+
+fun Position.findPath2(connections:Map<Position, List<Pair<Position,Int>>>, endPosition:Position, visited:Set<Pair<Position,Int>>, visitedPositions:Set<Position> ):List<Set<Pair<Position,Int>>> {
+    if (this == endPosition) {
+        val distance = visited.sumOf { it.second }
+        if (distance > max) { max = distance; println("end reaced $distance") }
+        return listOf(visited)
+    }
+    return connections.getValue(this).filter { it.first !in visitedPositions  }.flatMap{ pair ->
+        pair.first.findPath2(connections, endPosition, visited + pair, visitedPositions + pair.first )
+    }
+}
+
+fun Grid.findJunctions() =
+    filter{it.value != '#' && it.key.surroundingPositions(this).size > 2}.keys + listOf(this.startPostition(), endPostition())
+
+fun Set<Position>.findConnections(grid:Grid) =
+    associate{position -> Pair(position, position.findConnections(grid, this - position, setOf(position) ))}
+
+fun Position.findConnections(grid:Grid, otherJunctions:Set<Position>, visited:Set<Position> ):List<Pair<Position, Int>> {
+    if (this in otherJunctions) return listOf(Pair(this, visited.size - 1))
+    return surroundingPositions(grid).filter { it !in visited  }.flatMap{position ->
+        position.findConnections(grid, otherJunctions, visited + position )
+    }
+}
+
+fun List<String>.Grid2() = flatMapIndexed{ row, s -> s.replaceSlopes().mapIndexed { col, c -> Pair(Position(row, col),c)}}.toMap()
+
+fun String.replaceSlopes() = replace("<",".").replace(">",".").replace("^",".").replace("v",".")
--- day23/src/test/kotlin/MainTest.kt
+++ day23/src/test/kotlin/MainTest.kt
@@ -2,14 +2,87 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+    val testdata = """
+        #.#####################
+        #.......#########...###
+        #######.#########.#.###
+        ###.....#.>.>.###.#.###
+        ###v#####.#v#.###.#.###
+        ###.>...#.#.#.....#...#
+        ###v###.#.#.#########.#
+        ###...#.#.#.......#...#
+        #####.#.#.#######.#.###
+        #.....#.#.#.......#...#
+        #.#####.#.#.#########v#
+        #.#...#...#...###...>.#
+        #.#.#v#######v###.###v#
+        #...#.>.#...>.>.#.###.#
+        #####v#.#.###v#.#.###.#
+        #.....#...#...#.#.#...#
+        #.#########.###.#.#.###
+        #...###...#...#...#.###
+        ###.###.#.###v#####v###
+        #...#...#.#.>.>.#.>.###
+        #.###.###.#.###.#.#v###
+        #.....###...###...#...#
+        #####################.#
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "start position in test data " {
+            testdata.Grid().startPostition() shouldBe Position(0,1)
+        }
+        "end position in test data " {
+            testdata.Grid().endPostition() shouldBe Position(22,21)
+        }
+        "part one using test data should be 94" {
+            partOne(testdata) shouldBe 94
+        }
+        "part one using sample data should be 2438" {
+//            partOne(sampleData) shouldBe 2438
         }
     })
     "part two" should ({
+        "find junctions" {
+            val grid = testdata.Grid2()
+            val junctions = grid.findJunctions()
+            junctions.size shouldBe 9
+        }
+        "find connection of first junction to other junction" {
+            val grid = testdata.Grid2()
+            val junctions = grid.findJunctions().toSet()
+            val first = junctions.first {it.row == 0}
+            first.findConnections(grid, junctions - first, setOf(first)) shouldBe listOf(Pair(Position(5,3), 15))
+        }
+        "find connection of junction(5,3) to other junction" {
+            val grid = testdata.Grid2()
+            val junctions = grid.findJunctions().toSet()
+            val row5col3 = Position(5,3)
+            row5col3.findConnections(grid, junctions - row5col3, setOf(row5col3)) shouldBe listOf(
+                Pair(Position(3, 11),22),
+                Pair(Position(0, 1),15),
+                Pair(Position(13, 5),22))
+        }
+        "find all connections" {
+            val grid = testdata.Grid2()
+            val junctions = grid.findJunctions().toSet()
+            val connections = junctions.findConnections(grid)
+            connections.size shouldBe 9
+        }
+        "find paths using test data" {
+            val grid = testdata.Grid2()
+            val junctions = grid.findJunctions().toSet()
+            val connections:Map<Position,List<Pair<Position, Int>>> = junctions.findConnections(grid)
+            val paths = grid.startPostition().findPath2(connections, grid.endPostition(), setOf(), setOf(grid.startPostition()))
+            paths.maxOf{ it.sumOf { it.second } } shouldBe 154
+        }
+        "part two with test data should be 154" {
+            max = 0
+            partTwo(testdata) shouldBe 154
+        }
         "part two should be 0" {
-            partTwo(sampleData) shouldBe 0
+            max = 0
+            partTwo(sampleData) shouldBe 6658
         }
     })
 })
