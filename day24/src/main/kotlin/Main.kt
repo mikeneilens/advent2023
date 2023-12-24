82115a10b2efa0ea2fcfe0c6f2bface6db94e706
Mike Neilens <mike.neilens@gmail.com>
2023-12-24 13:07:20 +0000

    day24 part one

--- day24/src/main/kotlin/Main.kt
+++ day24/src/main/kotlin/Main.kt
@@ -1,11 +1,51 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+
+fun partOne(sampleData:List<String>, validRange: ValidRange = ValidRange(7.0,27.0)) :Int {
+    val intersections = sampleData.map { it.toHailstone() }.process(listOf())
+    return intersections.count{it.inRange(validRange)}
 }
 
-fun partOne(sampleData:String) :Int {
-    return 0
+fun List<Hailstone>.process(intersections: List<Position>):List<Position> {
+    if (isEmpty()) return intersections
+    val hailstone = first()
+    val newIntersections = drop(1).mapNotNull{otherHailstone ->
+        if (!hailstone.isParallelWith(otherHailstone)) {
+            val intersection = hailstone.positionOfIntersection(otherHailstone)
+            if (hailstone.intersectionIsFuture(intersection) && otherHailstone.intersectionIsFuture(intersection))
+                intersection
+            else
+                null
+        } else null
+    }
+    return drop(1).process(intersections +newIntersections)
 }
-fun partTwo(sampleData:String):Int {
+
+data class Position(val x:Double, val y:Double) {
+    fun asText() = "Position(x=${x.format(2)}, y=${y.format(2)}"
+    fun inRange(validRange:ValidRange) = (x >= validRange.min && x <= validRange.max) && (y >= validRange.min && y <= validRange.max)
+}
+
+fun Double.format(scale: Int) = "%.${scale}f".format(this)
+
+data class Hailstone(val position:Position, val direction:Position) {
+    val a = direction.y
+    val b = - direction.x
+    val c = position.x * direction.y - position.y * direction.x
+
+    fun isParallelWith(other:Hailstone) = a * other.b == b * other.a
+
+    fun positionOfIntersection(other:Hailstone) = Position(xCrossesWith(other), yCrossesWith(other))
+    //x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
+    fun xCrossesWith(other: Hailstone) = (c * other.b - other.c * b) / (a * other.b - other.a * b)
+    //y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
+    fun yCrossesWith(other: Hailstone) = (other.c * a - c * other.a) / (a * other.b - other.a * b)
+
+    fun intersectionIsFuture(intersection:Position) = (intersection.x - position.x) * direction.x >= 0 && (intersection.y - position.y) * direction.y >= 0
+}
+
+data class ValidRange(val min:Double, val max:Double)
+fun String.toHailstone() = Hailstone( split(" @ ")[0].toPosition(), split(" @ ")[1].toPosition())
+fun String.toPosition() = Position( split(", ")[0].trim().toDouble(), split(", ")[1].trim().toDouble())
+
+fun partTwo(sampleData:List<String>):Int {
     return 0
 }
--- day24/src/test/kotlin/MainTest.kt
+++ day24/src/test/kotlin/MainTest.kt
@@ -2,9 +2,54 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+
+    val testdata = """
+        19, 13, 30 @ -2,  1, -2
+        18, 19, 22 @ -1, -1, -2
+        20, 25, 34 @ -2, -2, -4
+        12, 31, 28 @ -1, -2, -1
+        20, 19, 15 @  1, -5, -3
+    """.trimIndent().split("\n")
+
     "part one" should ({
-        "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+        "convert 19, 13, 30 @ -2,  1, -2 to a hailstone" {
+            "19, 13, 30 @ -2,  1, -2".toHailstone() shouldBe Hailstone(Position(19.0,13.0), Position(-2.0,1.0))
+        }
+        "Hailstone A: 19, 13, 30 @ -2, 1, -2, Hailstone B: 18, 19, 22 @ -1, -1, -2 cross at x=14.333, y=15.333" {
+            val hailstoneA = "19, 13, 30 @ -2, 1, -2".toHailstone()
+            val hailstoneB = "18, 19, 22 @ -1, -1, -2".toHailstone()
+            val intersection = hailstoneA.positionOfIntersection(hailstoneB)
+            intersection.asText() shouldBe Position(14.333, 15.333).asText()
+            hailstoneA.intersectionIsFuture(intersection) shouldBe true
+        }
+        "Hailstone A: 19, 13, 30 @ -2, 1, -2, Hailstone B: 20, 19, 15 @ 1, -5, -3 did not cross in the future"{
+            val hailstoneA = "19, 13, 30 @ -2, 1, -2".toHailstone()
+            val hailstoneB = "20, 19, 15 @ 1, -5, -3".toHailstone()
+            val intersection = hailstoneA.positionOfIntersection(hailstoneB)
+            hailstoneA.intersectionIsFuture(intersection) shouldBe false
+        }
+        "Hailstone A: 18, 19, 22 @ -1, -1, -2, Hailstone B: 20, 25, 34 @ -2, -2, -4 are parallel so never cross" {
+            val hailstoneA = "18, 19, 22 @ -1, -1, -2".toHailstone()
+            val hailstoneB = "20, 25, 34 @ -2, -2, -4".toHailstone()
+            hailstoneA.isParallelWith(hailstoneB) shouldBe true
+        }
+        "Hailstone A: 19, 13, 30 @ -2, 1, -2, Hailstone B: 20, 25, 34 @ -2, -2, -4 intersect inside the test ara" {
+            val hailstoneA = "19, 13, 30 @ -2, 1, -2".toHailstone()
+            val hailstoneB = "20, 25, 34 @ -2, -2, -4".toHailstone()
+            val intersection = hailstoneA.positionOfIntersection(hailstoneB)
+            intersection.inRange(ValidRange(7.0, 27.0)) shouldBe true
+        }
+        "Hailstone A: 19, 13, 30 @ -2, 1, -2, Hailstone B: 12, 31, 28 @ -1, -2, -1 intersect outside the test ara" {
+            val hailstoneA = "19, 13, 30 @ -2, 1, -2".toHailstone()
+            val hailstoneB = "12, 31, 28 @ -1, -2, -1".toHailstone()
+            val intersection = hailstoneA.positionOfIntersection(hailstoneB)
+            intersection.inRange(ValidRange(7.0, 27.0)) shouldBe false
+        }
+        "part one with testdata should be 2" {
+            partOne(testdata, ValidRange(7.0, 27.0)) shouldBe 2
+        }
+        "part one with sampleData should be 16812" {
+            partOne(sampleData, ValidRange(200000000000000.0, 400000000000000.0)) shouldBe 16812
         }
     })
     "part two" should ({
