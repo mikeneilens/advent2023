39cabad62965d2908363d5e66b9120f00eedc9d0
Mike Neilens <mike.neilens@gmail.com>
2024-01-26 19:40:07 +0000

    day 17 part one

--- day17/src/main/kotlin/Main.kt
+++ day17/src/main/kotlin/Main.kt
@@ -1,11 +1,62 @@
-fun main(args: Array<String>) {
-    partOne(sampleData)
-    partTwo(sampleData)
+fun partOne(sampleData:List<String>) :Int {
+    val chart = sampleData.toChart()
+    val crucible = Crucible(Position(0,0), Direction.Right, 0 )
+    val heatLoss = mutableMapOf<Crucible, Int>()
+    val end = Position(sampleData.first().lastIndex, sampleData.lastIndex)
+    return expandCrucibleQueueUntilEnd(listOf(crucible), heatLoss, chart, end)
 }
 
-fun partOne(sampleData:String) :Int {
+fun partTwo(sampleData:List<String>):Int {
     return 0
 }
-fun partTwo(sampleData:String):Int {
-    return 0
+
+typealias Chart = Map<Position, Int>
+
+fun List<String>.toChart() = flatMapIndexed{ row, s -> s.mapIndexed { col, c -> Pair(Position(col,row), c.toString().toInt())  }}.toMap()
+
+data class Position(val col:Int, val row:Int) {
+
+    val  reversed:Position get() = Position(-col, -row)
+
+    operator fun plus(other:Position) = Position(col + other.col, row + other.row)
 }
+
+data class Crucible(val position: Position, val direction: Direction, val steps:Int) {
+    fun validDirections(chart:Chart) = Direction.values()
+        .filter{(it != Direction.None && it.offset != direction.offset.reversed)
+                && (steps < 3 || it != direction)
+                && (position + it.offset in chart) }
+}
+
+enum class Direction(val offset:Position) {
+    Right(Position(row = 0, col = +1)),
+    Left(Position(row = 0, col = -1)),
+    Up(Position(row = -1, col = 0)),
+    Down(Position(row = +1, col = 0)),
+    None(Position(row = 0, col = 0)),
+}
+
+fun expandCrucibleQueue(crucibleQueue:List<Crucible>, heatLoss:MutableMap<Crucible, Int>, chart:Chart):List<Crucible>  {
+    val crucible = crucibleQueue.first()
+    val newDirections = crucible.validDirections(chart)
+    val newCrucibles = newDirections.mapNotNull{ newDirection ->
+        val newPosition = crucible.position + newDirection.offset
+        val newSteps = if (newDirection == crucible.direction) crucible.steps + 1 else 1
+        val newCrucible = Crucible(newPosition, newDirection, newSteps)
+        if (newCrucible !in heatLoss) {
+            heatLoss[newCrucible] = heatLoss.getOrDefault(crucible,0) + chart.getValue(newPosition)
+            newCrucible }
+        else null
+    }
+    return (crucibleQueue.drop(1) + newCrucibles).sortedBy { heatLoss[it] }
+}
+
+tailrec fun expandCrucibleQueueUntilEnd(crucibleQueue: List<Crucible>, heatLoss:MutableMap<Crucible, Int>, chart:Chart, end:Position):Int {
+    if (end == crucibleQueue.first().position || crucibleQueue.isEmpty()) return heatLoss.minHeatLoss(end)
+    val newCrucibleQueue = expandCrucibleQueue(crucibleQueue, heatLoss, chart)
+    return expandCrucibleQueueUntilEnd(newCrucibleQueue, heatLoss, chart, end)
+}
+
+fun Map<Crucible, Int>.minHeatLoss(position:Position) = toList().filter{it.first.position == position}.minOf { it.second }
+
+
--- day17/src/test/kotlin/MainTest.kt
+++ day17/src/test/kotlin/MainTest.kt
@@ -2,9 +2,82 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest:WordSpec({
+
+    val testdata = """
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
+    """.trimIndent().split("\n")
+
     "part one" should ({
+        "mapping testdata to grid gives a first row of 2,4,1,3,4,3,2,3,1,1,3,2,3" {
+            testdata.toChart().filter { it.key.row == 0 }.values shouldBe listOf(2,4,1,3,4,3,2,3,1,1,3,2,3)
+            testdata.toChart().filter { it.key.row == 12 }.values shouldBe listOf(4,3,2,2,6,7,4,6,5,5,5,3,3)
+        }
+        "if crucible direction is Right and steps < 3 valid new directions are Up ,Down, Right" {
+            val chart = testdata.toChart()
+            val crucible = Crucible(Position(1,1), Direction.Right, 2 )
+            crucible.validDirections(chart).toSet() shouldBe setOf(Direction.Right, Direction.Up, Direction.Down)
+        }
+        "if crucible direction is Right and steps = 3 valid new directions are Up ,Down" {
+            val chart = testdata.toChart()
+            val crucible = Crucible(Position(1,1), Direction.Right, 3 )
+            crucible.validDirections(chart).toSet() shouldBe setOf(Direction.Up, Direction.Down)
+        }
+        "if crucible direction is Right and steps < 3 and current position in top left corner valid new directions are Down, Right" {
+            val chart = testdata.toChart()
+            val crucible = Crucible(Position(0,0), Direction.Right, 2 )
+            crucible.validDirections(chart).toSet() shouldBe setOf(Direction.Right, Direction.Down)
+        }
+        "starting at top left, newCrucibleQueue should have crucibles with position (row=1 , col=0) and position(row=0, col=1) " {
+            val chart = testdata.toChart()
+            val crucible = Crucible(Position(0,0), Direction.Right, 0 )
+            val heatLoss = mutableMapOf<Crucible, Int>()
+            expandCrucibleQueue(listOf(crucible), heatLoss, chart) shouldBe listOf(
+                Crucible(Position(0,1), Direction.Down, 1),
+                Crucible(Position(1,0), Direction.Right, 1),
+            )
+            heatLoss.minHeatLoss(Position(0,1)) shouldBe 3
+            heatLoss.minHeatLoss(Position(1,0)) shouldBe 4
+        }
+        "starting at top left, newCrucibleQueue after two cycles should have crucibles with position (row=1 , col=0) and position(row=0, col=1) " {
+            val chart = testdata.toChart()
+            val crucible = Crucible(Position(0,0), Direction.Right, 0 )
+            val heatLoss = mutableMapOf<Crucible, Int>()
+            val cruciblequeue1 = expandCrucibleQueue(listOf(crucible), heatLoss, chart)
+            val cruciblequeue2 = expandCrucibleQueue(cruciblequeue1, heatLoss, chart)
+            cruciblequeue2 shouldBe listOf(
+                Crucible(position=Position(col=1, row=0), direction=Direction.Right, steps=1),
+                Crucible(position=Position(col=1, row=1), direction=Direction.Right, steps=1),
+                Crucible(position=Position(col=0, row=2), direction=Direction.Down, steps=2)
+            )
+            heatLoss.minHeatLoss(Position(col=1, row=0)) shouldBe 4
+            heatLoss.minHeatLoss(Position(col=1, row=1)) shouldBe 5
+            heatLoss.minHeatLoss(Position(col=0, row=2)) shouldBe 6
+        }
+        "find end of test data" {
+            val chart = testdata.toChart()
+            val crucible = Crucible(Position(0,0), Direction.Right, 0 )
+            val heatLoss = mutableMapOf<Crucible, Int>()
+            val end = Position(testdata.first().lastIndex, testdata.lastIndex)
+            expandCrucibleQueueUntilEnd(listOf(crucible), heatLoss, chart, end) shouldBe 102
+        }
+
+        "part one with test data" {
+            partOne(testdata) shouldBe 102
+        }
         "part one should be 0" {
-            partOne(sampleData) shouldBe 0
+//            partOne(sampleData) shouldBe 845
         }
     })
     "part two" should ({
